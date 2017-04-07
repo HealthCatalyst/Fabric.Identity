0ca746ae8fb9d3d0ec0a726629b6ce06138882d0
michael.vidal <michael.vidal@healthcatalyst.com>
2017-04-07 11:25:02 -0400

    Fixed up some styling and set to auto redirect back to app on logout.

--- Fabric.Identity.API/Quickstart/Account/AccountOptions.cs
+++ Fabric.Identity.API/Quickstart/Account/AccountOptions.cs
@@ -13,7 +13,7 @@ namespace IdentityServer4.Quickstart.UI
         public static TimeSpan RememberMeLoginDuration = TimeSpan.FromDays(30);
 
         public static bool ShowLogoutPrompt = true;
-        public static bool AutomaticRedirectAfterSignOut = false;
+        public static bool AutomaticRedirectAfterSignOut = true;
 
         // to enable windows authentication, the host (IIS or IIS Express) also must have 
         // windows auth enabled.
