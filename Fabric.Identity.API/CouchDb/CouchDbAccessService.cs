bf9f5d62a63d77d8c96f0a2b2f433171acbde963
Kyle Paul <djjoebanks@gmail.com>
2017-05-01 16:11:33 -0700

    Added support for reading clients out of a client store backed by couchdb. added a class for working with couchdb. added configuration for couchdb.

---
+++ Fabric.Identity.API/CouchDb/CouchDbAccessService.cs
@@ -0,0 +1,68 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Fabric.Identity.API.Configuration;
+using Newtonsoft.Json;
+
+namespace Fabric.Identity.API.CouchDb
+{
+    public interface IDocumentDbService
+    {
+        Task<T> GetDocument<T>(string documentId);
+        void AddDocument<T>(string documentId, T documentObject);
+    }
+
+    public class CouchDbAccessService : IDocumentDbService
+    {
+        private readonly Serilog.ILogger _logger;
+        private readonly ICouchDbSettings _couchDbSettings;
+
+        private string GetDocumentUrl(string documentId)
+        {
+            return $"{_couchDbSettings.Server}{_couchDbSettings.DatabaseName}/{documentId}";
+        }
+
+        public CouchDbAccessService(ICouchDbSettings config, Serilog.ILogger logger)
+        {
+            _couchDbSettings = config;
+            _logger = logger;
+
+            _logger.Debug(
+                $"couchDb configuration properties: Server: {config.Server} -- DatabaseName: {config.DatabaseName}");
+        }
+
+        public Task<T> GetDocument<T>(string documentId)
+        {
+            HttpClient webClient = new HttpClient();
+            var response = webClient.GetAsync(GetDocumentUrl(documentId)).Result;
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return Task.FromResult(default(T));
+            }
+
+            var json = response.Content.ReadAsStringAsync().Result;
+            var client = JsonConvert.DeserializeObject<T>(json);
+
+            return Task.FromResult(client);
+        }
+
+        public void AddDocument<T>(string documentId, T documentObject)
+        {
+            if (GetDocument<T>(documentId).Result != null)
+                return;
+
+            HttpClient webClient = new HttpClient();
+
+            var postData = JsonConvert.SerializeObject(documentObject);
+            var content = new StringContent(postData, Encoding.UTF8, "application/json");
+            var response = webClient.PutAsync(GetDocumentUrl(documentId), content).Result;
+
+            if (!response.IsSuccessStatusCode)
+            {
+                _logger.Error($"could not add document to couchdb: documentId={documentId}");
+            }
+
+        }
+    }
+}
---
+++ Fabric.Identity.API/CouchDb/CouchDbClientStore.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Stores;
+
+namespace Fabric.Identity.API.CouchDb
+{
+    public class CouchDbClientStore : IClientStore
+    {
+        private readonly IDocumentDbService _documentDbService;
+
+        public CouchDbClientStore(IDocumentDbService documentDbService)
+        {
+            _documentDbService = documentDbService;
+        }
+
+        public Task<Client> FindClientByIdAsync(string clientId)
+        {
+            return _documentDbService.GetDocument<Client>(clientId);
+        }
+
+        //This is temporary
+        public void AddClients()
+        {
+            var clientList = new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "fabric-mvcsample",
+                    ClientName = "Sample Fabric MVC Client",
+                    AllowedGrantTypes = GrantTypes.Hybrid,
+
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    RedirectUris = {"http://localhost:5002/signin-oidc"},
+                    PostLogoutRedirectUris = {"http://localhost:5002/signout-callback-oidc"},
+
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "fabric.profile",
+                        "patientapi",
+                    },
+                    AllowOfflineAccess = true,
+                    RequireConsent = false
+                },
+                new Client
+                {
+                    ClientId = "fabric-angularsample",
+                    ClientName = "Sample Fabric Angular Client",
+                    AllowedGrantTypes = GrantTypes.Implicit,
+                    AllowAccessTokensViaBrowser = true,
+
+                    RedirectUris = {"http://localhost:4200/oidc-callback.html"},
+                    PostLogoutRedirectUris = {"http://localhost:4200"},
+                    AllowedCorsOrigins = {"http://localhost:4200"},
+
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "fabric.profile",
+                        "patientapi",
+                    },
+                    AllowOfflineAccess = true,
+                    RequireConsent = false
+                },
+                new Client
+                {
+                    ClientId = "fabric-sampleapi-client",
+                    ClientName = "Sample API Client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "fabric.profile",
+                        "patientapi",
+                    },
+                    RequireConsent = false
+                }
+            };
+
+            foreach (var client in clientList)
+            {
+                AddToCouchDb(client);
+
+            }
+        }
+
+        private void AddToCouchDb(Client client)
+        {
+            _documentDbService.AddDocument(client.ClientId, client);
+        }
+    }
+}
