8bbb417e9b9d40b526fdd00d47a622b1f3c4f994
michael.vidal <michael.vidal@healthcatalyst.com>
2017-06-15 10:40:48 -0400

    Added unit tests for token signing and cert rollover.

---
+++ Fabric.Identity.UnitTests/LinuxCertificateServiceTests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Fabric.Identity.API.Configuration;
+using Fabric.Identity.API.Services;
+using Fabric.Platform.Shared.Exceptions;
+using Moq;
+using Serilog;
+using Xunit;
+
+namespace Fabric.Identity.UnitTests
+{
+    public class LinuxCertificateServiceTests
+    {
+        private readonly Mock<ILogger> _mockLogger = new Mock<ILogger>();
+
+        [Theory, MemberData(nameof(SigningCredentialSettingsConfigurationException))]
+        public void GetCertificate_WithoutCertPath_ThrowsFabricConfigurationException(SigningCertificateSettings signingCertificateSettings, bool isPrimary)
+        {
+            var certificateService = new LinuxCertificateService(_mockLogger.Object);
+            Assert.Throws<FabricConfigurationException>(() => certificateService.GetCertificate(signingCertificateSettings, isPrimary));
+        }
+
+        [Theory, MemberData(nameof(SigningCredentialSettingsNotFoundException))]
+        public void GetCertificate_WithoutCertPath_ThrowsFileNotFoundException(SigningCertificateSettings signingCertificateSettings, bool isPrimary)
+        {
+            var certificateService = new LinuxCertificateService(_mockLogger.Object);
+            Assert.Throws<FileNotFoundException>(() => certificateService.GetCertificate(signingCertificateSettings, isPrimary));
+        }
+
+        public static IEnumerable<object[]> SigningCredentialSettingsConfigurationException => new[]
+        {
+            new object[]
+            {
+                new SigningCertificateSettings
+                {
+                    UseTemporarySigningCredential = false
+                },
+                true
+            },
+            new object[]
+            {
+                new SigningCertificateSettings
+                {
+                    UseTemporarySigningCredential = false,
+                    PrimaryCertificatePath = "somepath"
+                },
+                true
+            },new object[]
+            {
+                new SigningCertificateSettings
+                {
+                    UseTemporarySigningCredential = false,
+                    PrimaryCertificatePath = "somepath",
+                    PrimaryCertificatePasswordPath = "somepath"
+                },
+                false
+            },new object[]
+            {
+                new SigningCertificateSettings
+                {
+                    UseTemporarySigningCredential = false,
+                    PrimaryCertificatePath = "somepath",
+                    PrimaryCertificatePasswordPath = "somepath",
+                    SecondaryCertificatePath = "somepath"
+                },
+                false
+            }
+        };
+
+        public static IEnumerable<object[]> SigningCredentialSettingsNotFoundException => new[]
+        {
+            new object[]
+            {
+                new SigningCertificateSettings
+                {
+                    UseTemporarySigningCredential = false,
+                    PrimaryCertificatePath = "somepath",
+                    PrimaryCertificatePasswordPath = "somepath",
+                    SecondaryCertificatePath = "somepath",
+                    SecondaryCertificatePasswordPath = "somepath"
+                },
+                false
+            },new object[]
+            {
+                new SigningCertificateSettings
+                {
+                    UseTemporarySigningCredential = false,
+                    PrimaryCertificatePath = "somepath",
+                    PrimaryCertificatePasswordPath = "somepath"
+                },
+                true
+            }
+        };
+    }
+}
---
+++ Fabric.Identity.UnitTests/WindowsCertificateServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using Fabric.Identity.API.Configuration;
+using Fabric.Identity.API.Services;
+using Fabric.Platform.Shared.Exceptions;
+using IdentityModel;
+using Moq;
+using Serilog;
+using Xunit;
+
+namespace Fabric.Identity.UnitTests
+{
+    public class WindowsCertificateServiceTests
+    {
+        private readonly Mock<ILogger> _mockLogger = new Mock<ILogger>();
+
+        [Theory, MemberData(nameof(SigningCredentialSettings))]
+        public void GetCertificate_WithoutThumbprint_ThrowsException(SigningCertificateSettings signingCertificateSettings, bool isPrimary)
+        {
+            var certificateService = new WindowsCertificateService(_mockLogger.Object);
+            Assert.Throws<FabricConfigurationException>(() => certificateService.GetCertificate(signingCertificateSettings, isPrimary));
+        }
+
+        public static IEnumerable<object[]> SigningCredentialSettings => new[]
+        {
+            new object[]
+            {
+                new SigningCertificateSettings
+                {
+                    UseTemporarySigningCredential = false
+                },
+                true
+            },
+            new object[]
+            {
+                new SigningCertificateSettings
+                {
+                    UseTemporarySigningCredential = false,
+                    PrimaryCertificateThumbprint = "thumbprint"
+                },
+                false
+            },
+        };
+    }
+}
