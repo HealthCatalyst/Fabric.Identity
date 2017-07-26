8c9fb3b64aa3fd0f217eb724a43000baf008869e
michael.vidal <michael.vidal@healthcatalyst.com>
2017-07-26 09:57:12 -0400

    Skipping the hybrid flow test for now.

--- Fabric.Identity.FunctionalTests/tests/identitytests.js
+++ Fabric.Identity.FunctionalTests/tests/identitytests.js
@@ -310,7 +310,7 @@ describe("identity tests", function () {
             });
         });
 
-        it("should be able to authenticate user using hybrid flow", function(){
+        xit("should be able to authenticate user using hybrid flow", function(){
             this.timeout(10000);
              //setup custom phantomJS capability
             var phantomjsExe = require("phantomjs").path;
