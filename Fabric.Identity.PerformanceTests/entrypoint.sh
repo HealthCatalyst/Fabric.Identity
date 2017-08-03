8bd23cd16a9520812ffa6ec865a9e2cfafdfa981
Kyle Paul <djjoebanks@gmail.com>
2017-08-02 17:56:09 -0700

    added basic performance test for identity. added apdex calculator for identity

---
+++ Fabric.Identity.PerformanceTests/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+./create-userproperties.sh $AUTH_SERVER $AUTH_PORT $IDENTITY_SERVER $IDENTITY_PORT $BOB_PASSWORD $ALICE_PASSWORD
+mv user.properties /jmeter/apache-jmeter-3.2/bin/user.properties
+mkdir results
+mkdir results/output
+/jmeter/apache-jmeter-3.2/bin/jmeter -n -t /Fabric.Identity.Perf.jmx -l /results/results.txt -e -o /results/output
+cd /apdexcalc
+dotnet /apdexcalc/Fabric.Identity.ApdexCalculator.dll /results/results.txt
