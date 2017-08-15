8292a9756d532f32026e7c8a4a454614f7ec286b
Brian Smith <brian.smith@healthcatalyst.com>
2017-08-15 11:47:43 -0400

    change CRLF to LF in set-environment script

--- Fabric.Identity.API/scripts/set-environment.sh
+++ Fabric.Identity.API/scripts/set-environment.sh
@@ -3,4 +3,5 @@
 IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $IDENTITYCONTAINERNAME)
 echo "Identity Container running on $IP"
 echo "##vso[task.setvariable variable=FabricIdentityBaseUrl;]http://$IP:5001"
-echo "Identity Server URL: $FABRICIDENTITYBASEURL"
+echo "Identity Server URL (FABRICIDENTITYBASEURL): $FABRICIDENTITYBASEURL"
+echo "Identity Server URL (FabricIdentityBaseUrl): $FabricIdentityBaseUrl"
