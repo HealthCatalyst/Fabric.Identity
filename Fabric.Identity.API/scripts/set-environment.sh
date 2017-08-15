7c153ffc7db4d3845ea46d7125c92887104a7db6
Brian Smith <brian.smith@healthcatalyst.com>
2017-08-15 10:23:37 -0400

    fixed set-environment script for build server

--- Fabric.Identity.API/scripts/set-environment.sh
+++ Fabric.Identity.API/scripts/set-environment.sh
@@ -2,3 +2,5 @@
 
 IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $IDENTITYCONTAINERNAME)
 echo "Identity Container running on $IP"
+echo "##vso[task.setvariable variable=FabricIdentityBaseUrl;]http://$IP:5001"
+echo "Identity Server URL: $FABRICIDENTITYBASEURL"
