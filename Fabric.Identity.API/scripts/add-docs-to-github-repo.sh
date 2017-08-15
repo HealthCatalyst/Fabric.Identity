38409e0a6681cc5ea211313cf6ac22a3d2147c3c
Brian Smith <brian.smith@healthcatalyst.com>
2017-08-15 16:16:11 -0400

    added script to push markdown files to Github wiki repo

---
+++ Fabric.Identity.API/scripts/add-docs-to-github-repo.sh
@@ -0,0 +1,6 @@
+﻿#!/bin/bash
+
+git clone https://$GITWIKIACCESSTOKEN@github.com/HealthCatalyst/Fabric.Identity.wiki.git
+git add $Build.ArtifactStagingDirectory/*.md
+git commit -m 'update API documentation'
+git push origin master
