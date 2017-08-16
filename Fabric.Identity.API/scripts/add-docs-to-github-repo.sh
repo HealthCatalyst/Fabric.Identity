aff87cb15a2a48751bf2f8d9694a96fce99c8f1d
Brian Smith <brian.smith@healthcatalyst.com>
2017-08-16 10:56:57 -0400

    move MD files to the Git repo directory on the VSTS agent

--- Fabric.Identity.API/scripts/add-docs-to-github-repo.sh
+++ Fabric.Identity.API/scripts/add-docs-to-github-repo.sh
@@ -1,6 +1,8 @@
 ﻿#!/bin/bash
 
 git clone https://$GITWIKIACCESSTOKEN@github.com/HealthCatalyst/Fabric.Identity.wiki.git
-git add $Build.ArtifactStagingDirectory/*.md
+
+mv $Build.ArtifactStagingDirectory/*.md Fabric.Identity.wiki
+git add Fabric.Identity.wiki/*.md
 git commit -m 'update API documentation'
 git push origin master
