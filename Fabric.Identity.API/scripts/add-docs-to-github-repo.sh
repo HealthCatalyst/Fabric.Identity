e27c1fec5083a416f450e09607ff0515c62c63e1
Brian Smith <brian.smith@healthcatalyst.com>
2017-08-16 14:06:37 -0400

    fixed mv command in script

--- Fabric.Identity.API/scripts/add-docs-to-github-repo.sh
+++ Fabric.Identity.API/scripts/add-docs-to-github-repo.sh
@@ -2,7 +2,7 @@
 
 git clone https://$GITWIKIACCESSTOKEN@github.com/HealthCatalyst/Fabric.Identity.wiki.git
 
-mv $Build.ArtifactStagingDirectory/*.md Fabric.Identity.wiki
+mv *.md Fabric.Identity.wiki
 git add Fabric.Identity.wiki/*.md
 git commit -m 'update API documentation'
 git push origin master
