218cbcbfee4e03c7c521a042f739f05344a8e82f
Brian Smith <brian.smith@healthcatalyst.com>
2017-08-16 14:39:24 -0400

    1) added logging to script and 2) cd to Fabric.Identity.wiki dir before executing Git commands

--- Fabric.Identity.API/scripts/add-docs-to-github-repo.sh
+++ Fabric.Identity.API/scripts/add-docs-to-github-repo.sh
@@ -1,8 +1,16 @@
 ﻿#!/bin/bash
 
+echo "Cloning Git Wiki repo..."
 git clone https://$GITWIKIACCESSTOKEN@github.com/HealthCatalyst/Fabric.Identity.wiki.git
 
+echo "Moving MD files to Fabric.Identity.wiki..."
 mv *.md Fabric.Identity.wiki
-git add Fabric.Identity.wiki/*.md
+
+echo "Changing directory..."
+cd Fabric.Identity.wiki
+
+echo "Present directory = $(pwd)"
+
+git add *.md
 git commit -m 'update API documentation'
 git push origin master
