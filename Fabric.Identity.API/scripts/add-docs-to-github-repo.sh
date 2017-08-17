bd0f01894ff545a3119d01a1279f042fb9e54e8c
Brian Smith <brian.smith@healthcatalyst.com>
2017-08-16 21:56:37 -0400

    1) put REPO variable in quotes, 2) rename *.md files to file name that matches links in Wiki sidebar

--- Fabric.Identity.API/scripts/add-docs-to-github-repo.sh
+++ Fabric.Identity.API/scripts/add-docs-to-github-repo.sh
@@ -1,9 +1,15 @@
 ﻿#!/bin/bash
-
 echo "Cloning Git Wiki repo..."
-git clone https://$GITWIKIACCESSTOKEN@github.com/HealthCatalyst/Fabric.Identity.wiki.git
+REPO="https://$GITWIKIACCESSTOKEN@github.com/HealthCatalyst/Fabric.Identity.wiki.git"
+echo $REPO
+git clone $REPO
 
 echo "Moving MD files to Fabric.Identity.wiki..."
+mv overview.md API-Reference-Overview.md
+mv paths.md API-Reference-Resources.md
+mv definitions.md API-Reference-Models.md
+mv security.md API-Reference-Security.md 
+
 mv *.md Fabric.Identity.wiki
 
 echo "Changing directory..."
@@ -11,7 +17,7 @@ cd Fabric.Identity.wiki
 
 echo "-----Present directory = $(pwd)-----"
 
-git config user.name "Fabric Identity System User"
+git config user.name "VSTS System User"
 git config user.email "brian.smith@healthcatalyst.com"
 git add *.md
 git commit -m 'update API documentation'
--- Fabric.Identity.API/scripts/set-environment.sh
+++ Fabric.Identity.API/scripts/set-environment.sh
@@ -1,5 +1,4 @@
 ﻿#!/bin/bash
-
 IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $IDENTITYCONTAINERNAME)
 echo "Identity Container running on $IP"
 echo "##vso[task.setvariable variable=FabricIdentityBaseUrl;]http://$IP:5001"
