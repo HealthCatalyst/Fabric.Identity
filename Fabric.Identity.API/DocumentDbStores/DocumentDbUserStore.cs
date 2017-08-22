4a948adbfdbf29d71d80fcdffb09d0f4d56e2c9b
Kyle Paul <djjoebanks@gmail.com>
2017-08-21 18:18:12 -0700

    added method for adding a new user with the claims from the external provider

--- Fabric.Identity.API/DocumentDbStores/DocumentDbUserStore.cs
+++ Fabric.Identity.API/DocumentDbStores/DocumentDbUserStore.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Fabric.Identity.API.Models;
 using Fabric.Identity.API.Services;
+using IdentityModel;
 using Serilog;
 
 namespace Fabric.Identity.API.DocumentDbStores
@@ -19,24 +22,74 @@ namespace Fabric.Identity.API.DocumentDbStores
             _logger = logger;
         }
 
-        public Task<User> FindBySubjectId(string subjectId)
-        {
-            //store users by subjectId in couchDb ???
+        public async Task<User> FindBySubjectId(string subjectId)
+        {            
+            var user = await _documentDbService.GetDocument<User>(subjectId);
 
-            throw new NotImplementedException();
+            return user;
         }
 
         public Task<User> FindByExternalProvider(string provider, string subjectId)
         {
-            //userid is the sub from the claim or potentially the name identifier 
+            //create a view in couchDb for finding by subject and provider ??
             throw new NotImplementedException();
         }
 
         public Task<User> ProvisionUser(string provider, string subjectId, IEnumerable<Claim> claims)
         {
-            //create a new User and store the users claims
+            // create a list of claims that we want to transfer into our store
+            var filtered = new List<Claim>();
 
-            throw new NotImplementedException();
+            foreach (var claim in claims)
+            {
+                // if the external system sends a display name - translate that to the standard OIDC name claim
+                if (claim.Type == ClaimTypes.Name)
+                {
+                    filtered.Add(new Claim(JwtClaimTypes.Name, claim.Value));
+                }
+                // if the JWT handler has an outbound mapping to an OIDC claim use that
+                else if (JwtSecurityTokenHandler.DefaultOutboundClaimTypeMap.ContainsKey(claim.Type))
+                {
+                    filtered.Add(new Claim(JwtSecurityTokenHandler.DefaultOutboundClaimTypeMap[claim.Type], claim.Value));
+                }
+                // copy the claim as-is
+                else
+                {
+                    filtered.Add(claim);
+                }
+            }
+
+            // if no display name was provided, try to construct by first and/or last name
+            if (filtered.All(x => x.Type != JwtClaimTypes.Name))
+            {
+                var first = filtered.FirstOrDefault(x => x.Type == JwtClaimTypes.GivenName)?.Value;
+                var last = filtered.FirstOrDefault(x => x.Type == JwtClaimTypes.FamilyName)?.Value;
+                if (first != null && last != null)
+                {
+                    filtered.Add(new Claim(JwtClaimTypes.Name, first + " " + last));
+                }
+                else if (first != null)
+                {
+                    filtered.Add(new Claim(JwtClaimTypes.Name, first));
+                }
+                else if (last != null)
+                {
+                    filtered.Add(new Claim(JwtClaimTypes.Name, last));
+                }
+            }
+
+            // create new user
+            var user = new User
+            {
+                SubjectId = subjectId,
+                ProviderName = provider,
+                Claims = filtered
+            };
+
+            _documentDbService.AddDocument(subjectId, user);
+
+            return Task.FromResult(user);
+
         }
     }
 }
