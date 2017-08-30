15f1cfa251ef9c4b4c5286e4ea4edbc43569ad29
michael.vidal <michael.vidal@healthcatalyst.com>
2017-08-30 11:52:25 -0400

    Simplified the Identity windows installation script and modified it to use the common powershell module.

--- Fabric.Identity.API/scripts/Install-Identity-Windows.ps1
+++ Fabric.Identity.API/scripts/Install-Identity-Windows.ps1
@@ -3,153 +3,40 @@
 #
 param(
 	[String]$zipPackage, 
-	[String]$webroot, 
-	[String]$appName, 
-	[String]$iisUser, 
-	[int]$portNumber, 
-	[String]$hostHeader, 
+	[String]$webroot = "C:\inetpub\wwwroot", 
+	[String]$appName = "identity", 
+	[String]$iisUser = "IIS_IUSRS", 
 	[String]$sslCertificateThumbprint,
-	[String]$clientName,
-	[String]$primarySigningCertificateThumbprint,
-	[String]$secondarySigningCertificateThumbprint,
 	[String]$couchDbServer,
 	[String]$couchDbUsername,
 	[String]$couchDbPassword,
 	[String]$appInsightsInstrumentationKey,
-	[String]$siteName)
+	[String]$siteName = "Default Web Site",
+	[String]$hostUrl)
 
-Import-Module WebAdministration
-Add-Type -AssemblyName System.IO.Compression.FileSystem
-
-function Add-EnvironmentVariable($variableName, $variableValue, $config){
-	Write-Host "Writing $variableName to config"
-	$environmentVariablesNode = $config.configuration.'system.webServer'.aspNetCore.environmentVariables
-	$environmentVariable = $config.CreateElement("environmentVariable")
-	
-	$nameAttribute = $config.CreateAttribute("name")
-	$nameAttribute.Value = $variableName
-	$environmentVariable.Attributes.Append($nameAttribute)
-	
-	$valueAttribute = $config.CreateAttribute("value")
-	$valueAttribute.Value = $variableValue
-	$environmentVariable.Attributes.Append($valueAttribute)
-
-	$environmentVariablesNode.AppendChild($environmentVariable)
-}
-
-function Create-AppRoot($appDirectory, $iisUser){
-	# Create the necessary directories for the app
-	$logDirectory = "$appDirectory\logs"
-
-	Write-Host "Creating application directory: $appDirectory."
-	if(!(Test-Path $appDirectory)) {mkdir $appDirectory}
-
-	Write-Host "Creating applciation log directory: $logDirectory."
-	if(!(Test-Path $logDirectory)) {
-		mkdir $logDirectory
-		Write-Host "Setting Write and Read access for $iisUser on $logDirectory."
-		$acl = (Get-Item $logDirectory).GetAccessControl('Access')
-		$writeAccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule($iisUser, "Write", "ContainerInherit,ObjectInherit", "None", "Allow")
-		$readAccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule($iisUser, "Read", "ContainerInherit,ObjectInherit", "None", "Allow")
-		$acl.AddAccessRule($writeAccessRule)
-		$acl.AddAccessRule($readAccessRule)
-		Set-Acl -Path $logDirectory $acl
-	}
-}
-
-function Create-AppPool($appName){
-	cd IIS:\AppPools
-
-	if(!(Test-Path $appName -PathType Container))
-	{
-		Write-Host "AppPool $appName does not exist...creating."
-		$appPool = New-WebAppPool $appName
-		$appPool | Set-ItemProperty -Name "managedRuntimeVersion" -Value ""
-		$appPool.Start()
-	}
-}
-
-function Create-WebSite($appName, $portNumber, $appDirectory, $hostHeader){
-	cd IIS:\Sites
-
-	if(!(Test-Path $appName -PathType Container))
-	{
-		Write-Host "WebSite $appName does not exist...creating."
-		$webSite = New-Website -Name $appName -Port $portNumber -Ssl -PhysicalPath $appDirectory -ApplicationPool $appName -HostHeader $hostHeader
-	
-		Write-Host "Assigning certificate..."
-		$cert = Get-Item Cert:\LocalMachine\My\$sslCertificateThumbprint
-		cd IIS:\SslBindings
-		$sslBinding = "0.0.0.0!$portNumber"
-		if(!(Test-Path $sslBinding)){
-			$cert | New-Item $sslBinding
-		}
-	}
-}
-
-function Create-Application($appName, $siteName, $appDirectory){
-	cd IIS:\
-
-	New-WebApplication -Name $appName -Site $siteName -PhysicalPath $appDirectory -ApplicationPool $appName
-}
-
-function Publish-WebSite($zipPackage, $appDirectory){
-	# Extract the app into the app directory
-	Write-Host "Extracting $zipPackage to $appDirectory."
-	[System.IO.Compression.ZipFile]::ExtractToDirectory($zipPackage, $appDirectory)
-	#Start-Sleep -Seconds 3
-}
-
-function Set-EnvironmentVariables($appDirectory, $environmentVariables){
-	Write-Host "Writing environment variables to config..."
-	$webConfig = [xml](Get-Content $appDirectory\web.config)
-	foreach ($variable in $environmentVariables.GetEnumerator()){
-		Add-EnvironmentVariable $variable.Name $variable.Value $webConfig
-	}
-
-	$webConfig.Save("$appDirectory\web.config")
-}
-
-function Encrypt-String($signingCert, $stringToEncrypt){
-	$encryptedString = [System.Convert]::ToBase64String($signingCert.PublicKey.Key.Encrypt([System.Text.Encoding]::UTF8.GetBytes($stringToEncrypt), $true))
-	return "!!enc!!:" + $encryptedString
-}
-
-# Install the .net core windows server hosting bundle
-#Write-Host "Installing dotnet core Windows Server hosting bundle..."
-#Invoke-WebRequest -Uri https://go.microsoft.com/fwlink/?linkid=844461 -OutFile bundle.exe
-#.\bundle.exe /quiet /install
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/HealthCatalyst/InstallScripts/master/common/Fabric-Install-Utilities.psm1 -OutFile Fabric-Install-Utilities.psm1
+Import-Module -Name .\Fabric-Install-Utilities.psm1 -Verbose
 
 $appDirectory = "$webroot\$appName"
-Create-AppRoot $appDirectory $iisUser
+New-AppRoot $appDirectory $iisUser
 Write-Host "App directory is: $appDirectory"
-Create-AppPool $appName
-
-if($siteName){
-	Create-Application $appName $siteName $appDirectory
-}else{
-	Create-WebSite $appName $portNumber $appDirectory $hostHeader
-}
-
+New-AppPool $appName
+New-App $appName $siteName $appDirectory
 Publish-WebSite $zipPackage $appDirectory
 
 
 #Write environment variables
 Write-Host "Loading up environment variables..."
 $environmentVariables = @{"HostingOptions__UseInMemoryStores" = "false"; "HostingOptions__UseTestUsers" = "false"; "AllowLocalLogin" = "false"}
-$signingCert = Get-Item Cert:\LocalMachine\My\$primarySigningCertificateThumbprint
+$signingCert = Get-Item Cert:\LocalMachine\My\$sslCertificateThumbprint
 
 if($clientName){
 	$environmentVariables.Add("ClientName", $clientName)
 }
 
-if ($primarySigningCertificateThumbprint){
+if ($sslCertificateThumbprint){
 	$environmentVariables.Add("SigningCertificateSettings__UseTemporarySigningCredential", "false")
-	$environmentVariables.Add("SigningCertificateSettings__PrimaryCertificateThumbprint", $primarySigningCertificateThumbprint)
-}
-
-if ($secondarySigningCertificateThumbprint){
-	$environmentVariables.Add("SigningCertificateSettings__SecondaryCertificateThumbprint", $secondarySigningCertificateThumbprint)
+	$environmentVariables.Add("SigningCertificateSettings__PrimaryCertificateThumbprint", $sslCertificateThumbprint)
 }
 
 if ($couchDbServer){
@@ -161,7 +48,7 @@ if ($couchDbUsername){
 }
 
 if ($couchDbPassword){
-	$encryptedCouchDbPassword = Encrypt-String $signingCert $couchDbPassword
+	$encryptedCouchDbPassword = Get-EncryptedString $signingCert $couchDbPassword
 	$environmentVariables.Add("CouchDbSettings__Password", $encryptedCouchDbPassword)
 }
 
@@ -170,6 +57,6 @@ if($appInsightsInstrumentationKey){
 	$environmentVariables.Add("ApplicationInsights__InstrumentationKey", $appInsightsInstrumentationKey)
 }
 
-$environmentVariables.Add("IdentityServerConfidentialClientSettings__Authority", "https://${hostHeader}:${portNumber}")
+$environmentVariables.Add("IdentityServerConfidentialClientSettings__Authority", "${hostUrl}/${appName}")
 
 Set-EnvironmentVariables $appDirectory $environmentVariables
