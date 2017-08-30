3467d9d21281fd7590466b35750a5bc790c1c88a
Kyle Paul <djjoebanks@gmail.com>
2017-08-30 13:57:48 -0700

    added some logging

--- Fabric.Identity.API/Quickstart/Account/AccountController.cs
+++ Fabric.Identity.API/Quickstart/Account/AccountController.cs
@@ -22,6 +22,7 @@ using Fabric.Identity.API.Services;
 using Microsoft.AspNetCore.Authentication;
 using IdentityServer4.Events;
 using IdentityServer4.Extensions;
+using Serilog;
 
 namespace IdentityServer4.Quickstart.UI
 {
@@ -37,6 +38,7 @@ namespace IdentityServer4.Quickstart.UI
         private readonly IIdentityServerInteractionService _interaction;
         private readonly IEventService _events;
         private readonly IAppConfiguration _appConfiguration;
+        private readonly ILogger _logger;
         private readonly AccountService _account;
         private readonly UserFunctions _userFunctions;
 
@@ -47,6 +49,7 @@ namespace IdentityServer4.Quickstart.UI
             IEventService events,
             IAppConfiguration appConfiguration,
             DocumentDbUserStore documentDbUserStore,
+            ILogger logger,
             TestUserStore users = null)
         {
             // if the TestUserStore is not in DI, then we'll just use the global users collection
@@ -54,6 +57,7 @@ namespace IdentityServer4.Quickstart.UI
             _interaction = interaction;
             _events = events;
             _appConfiguration = appConfiguration;
+            _logger = logger;
             _account = new AccountService(interaction, httpContextAccessor, clientStore, appConfiguration);
             _userFunctions = new UserFunctions(_users, documentDbUserStore);
             
@@ -141,10 +145,10 @@ namespace IdentityServer4.Quickstart.UI
         {
             returnUrl = Url.Action("ExternalLoginCallback", new { returnUrl = returnUrl });
 
-            // windows authentication is modeled as external in the asp.net core authentication manager, so we need special handling
+            _logger.Information("windows authentication is modeled as external in the asp.net core authentication manager, so we need special handling");
             if (AccountOptions.WindowsAuthenticationSchemes.Contains(provider))
             {
-                // but they don't support the redirect uri, so this URL is re-triggered when we call challenge
+                _logger.Information("but they don't support the redirect uri, so this URL is re-triggered when we call challenge"); 
                 if (HttpContext.User is WindowsPrincipal wp)
                 {
                     var props = new AuthenticationProperties();
@@ -154,7 +158,7 @@ namespace IdentityServer4.Quickstart.UI
                     id.AddClaim(new Claim(JwtClaimTypes.Subject, HttpContext.User.Identity.Name));
                     id.AddClaim(new Claim(JwtClaimTypes.Name, HttpContext.User.Identity.Name));
 
-                    // add the groups as claims -- be careful if the number of groups is too large
+                    _logger.Information("add the groups as claims -- be careful if the number of groups is too large");
                     if (AccountOptions.IncludeWindowsGroups)
                     {
                         var wi = wp.Identity as WindowsIdentity;
@@ -168,13 +172,13 @@ namespace IdentityServer4.Quickstart.UI
                 }
                 else
                 {
-                    // this triggers all of the windows auth schemes we're supporting so the browser can use what it supports
+                    _logger.Information("this triggers all of the windows auth schemes we're supporting so the browser can use what it supports");
                     return new ChallengeResult(AccountOptions.WindowsAuthenticationSchemes);
                 }
             }
             else
             {
-                // start challenge and roundtrip the return URL
+                _logger.Information("start challenge and roundtrip the return URL");
                 var props = new AuthenticationProperties
                 {
                     RedirectUri = returnUrl,
@@ -190,7 +194,7 @@ namespace IdentityServer4.Quickstart.UI
         [HttpGet]
         public async Task<IActionResult> ExternalLoginCallback(string returnUrl)
         {
-            // read external identity from the temporary cookie
+            _logger.Information("read external identity from the temporary cookie");
             var info = await HttpContext.Authentication.GetAuthenticateInfoAsync(IdentityServerConstants.ExternalCookieAuthenticationScheme);
             var tempUser = info?.Principal;
             if (tempUser == null)
@@ -198,11 +202,11 @@ namespace IdentityServer4.Quickstart.UI
                 throw new Exception("External authentication error");
             }
 
-            // retrieve claims of the external user
+            _logger.Information("retrieve claims of the external user");
             var claims = tempUser.Claims.ToList();
 
-            // try to determine the unique id of the external user - the most common claim type for that are the sub claim and the NameIdentifier
-            // depending on the external provider, some other claim type might be used
+            _logger.Information("try to determine the unique id of the external user - the most common claim type for that are the sub claim and the NameIdentifier");
+            _logger.Information("depending on the external provider, some other claim type might be used");
             var userIdClaim = claims.FirstOrDefault(x => x.Type == JwtClaimTypes.Subject);
             if (userIdClaim == null)
             {
@@ -213,8 +217,8 @@ namespace IdentityServer4.Quickstart.UI
                 throw new Exception("Unknown userid");
             }
 
-            // remove the user id claim from the claims collection and move to the userId property
-            // also set the name of the external authentication provider
+            _logger.Information("remove the user id claim from the claims collection and move to the userId property");
+            _logger.Information("also set the name of the external authentication provider");
             claims.Remove(userIdClaim);
             var provider = info.Properties.Items["scheme"];
             var userId = userIdClaim.Value;
@@ -222,59 +226,59 @@ namespace IdentityServer4.Quickstart.UI
             UserInfo userInfo;
             if (_appConfiguration.HostingOptions.UseTestUsers)
             {
-                // check if the external user is already provisioned
+                _logger.Information("check if the external user is already provisioned");
                 var user = _userFunctions.FindTestUserByExternalProvider(provider, userId);
                 if (user == null)
                 {
-                    // this sample simply auto-provisions new external user
-                    // another common approach is to start a registrations workflow first
+                    _logger.Information("this sample simply auto-provisions new external user");
+                    _logger.Information("another common approach is to start a registrations workflow first");
                     user = _userFunctions.AddTestUser(provider, userId, claims);
                 }
                 else
                 {
-                    //update the role claims from the provider
+                    _logger.Information("update the role claims from the provider");
                     _userFunctions.UpdateTestUserRoleClaims(user, claims);
                 }
                 userInfo = new UserInfo(user);
             }
             else
             {
-                // check if the external user is already provisioned
+                _logger.Information("check if the external user is already provisioned");
                 var user = await _userFunctions.FindUserByExternalProvider(provider, userId);
                 if (user == null)
                 {
-                    // this sample simply auto-provisions new external user
-                    // another common approach is to start a registrations workflow first
+                    _logger.Information("this sample simply auto-provisions new external user");
+                    _logger.Information("another common approach is to start a registrations workflow first");
                     user =  _userFunctions.AddUser(provider, userId, claims);                   
                 }
                 else
                 {
-                    //update the role claims from the provider
+                    _logger.Information("update the role claims from the provider");
                     _userFunctions.UpdateUserRoleClaims(user, claims);
                 }
                 userInfo = new UserInfo(user);
-                //update the user model with the login
+                _logger.Information("update the user model with the login");
                 var context = await _interaction.GetAuthorizationContextAsync(returnUrl);
                 await _userFunctions.SetLastLogin(context.ClientId, userInfo.SubjectId);
             }
 
             var additionalClaims = new List<Claim>();
 
-            // if the external system sent a session id claim, copy it over
+            _logger.Information("if the external system sent a session id claim, copy it over");
             var sid = claims.FirstOrDefault(x => x.Type == JwtClaimTypes.SessionId);
             if (sid != null)
             {
                 additionalClaims.Add(new Claim(JwtClaimTypes.SessionId, sid.Value));
             }
 
-            // if the external provider issues groups claims, copy it over
+            _logger.Information("if the external provider issues groups claims, copy it over");
             var groupClaims = claims.Where(c => c.Type == "groups").ToList();
             if (groupClaims.Any())
             {
                 additionalClaims.AddRange(groupClaims);
             }
 
-            // if the external provider issued an id_token, we'll keep it for signout
+            _logger.Information("if the external provider issued an id_token, we'll keep it for signout");
             AuthenticationProperties props = null;
             var id_token = info.Properties.GetTokenValue("id_token");
             if (id_token != null)
@@ -283,14 +287,14 @@ namespace IdentityServer4.Quickstart.UI
                 props.StoreTokens(new[] { new AuthenticationToken { Name = "id_token", Value = id_token } });
             }
 
-            // issue authentication cookie for user
+            _logger.Information("issue authentication cookie for user");
             await _events.RaiseAsync(new UserLoginSuccessEvent(provider, userId, userInfo.SubjectId, userInfo.Username));
             await HttpContext.Authentication.SignInAsync(userInfo.SubjectId, userInfo.Username, provider, props, additionalClaims.ToArray());
 
-            // delete temporary cookie used during external authentication
+            _logger.Information("delete temporary cookie used during external authentication");
             await HttpContext.Authentication.SignOutAsync(IdentityServerConstants.ExternalCookieAuthenticationScheme);
 
-            // validate return URL and redirect back to authorization endpoint or a local page
+            _logger.Information("validate return URL and redirect back to authorization endpoint or a local page");
             if (_interaction.IsValidReturnUrl(returnUrl) || Url.IsLocalUrl(returnUrl))
             {
                 return Redirect(returnUrl);
