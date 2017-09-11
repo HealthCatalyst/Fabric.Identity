e5a8f1c34ed29afbcdccd0ccaf351724685efa0e
Kyle Paul <djjoebanks@gmail.com>
2017-09-11 11:29:21 -0700

    added authorization handler for the Read scope, use it for controlling authorization on the users controller

---
+++ Fabric.Identity.API/Authorization/ReadAuthorizationHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Fabric.Identity.API.Configuration;
+using IdentityModel;
+using Microsoft.AspNetCore.Authorization;
+using Serilog;
+
+namespace Fabric.Identity.API.Authorization
+{
+    public class ReadAuthorizationHandler : AuthorizationHandler<ReadScopeRequirement>
+    {
+        private readonly ILogger _logger;
+        private readonly IAppConfiguration _appConfiguration;
+
+        public ReadAuthorizationHandler(IAppConfiguration appConfiguration, ILogger logger)
+        {
+            _logger = logger;
+            _appConfiguration = appConfiguration ?? throw new ArgumentNullException(nameof(appConfiguration)); ;
+        }
+
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ReadScopeRequirement requirement)
+        {
+            //ensure the logged in client has the read scope claim
+
+            if (HasRequiredScopeClaim(context.User, requirement.ReadScope))
+            {
+                _logger.Information($"User has required scope claim: {requirement.ReadScope}, authorization succeeded.");
+                context.Succeed(requirement);                
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private bool HasRequiredScopeClaim(ClaimsPrincipal user, string claimType)
+        {
+            var hasScopeClaim = user.Claims.Any(c => c.Type == JwtClaimTypes.Scope &&
+                                                     c.Value == claimType &&
+                                                     c.Issuer == _appConfiguration.IssuerUri);
+            return hasScopeClaim;
+        }
+    }
+}
---
+++ Fabric.Identity.API/Authorization/ReadScopeRequirement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Primitives;
+
+namespace Fabric.Identity.API.Authorization
+{
+    public class ReadScopeRequirement : IAuthorizationRequirement
+    {
+        public readonly string ReadScope = FabricIdentityConstants.IdentityReadScope;
+    }
+}
