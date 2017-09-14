575ffd573a9963bd2f57498e409315f812c0d521
Kyle Paul <djjoebanks@gmail.com>
2017-09-13 18:21:19 -0700

    put back authorize attribute

--- Fabric.Identity.API/Management/UsersController.cs
+++ Fabric.Identity.API/Management/UsersController.cs
@@ -11,8 +11,8 @@ using Serilog;
 
 namespace Fabric.Identity.API.Management
 {
-    [Authorize(Policy = "RegistrationThreshold", ActiveAuthenticationSchemes = "Bearer")]
-    //   [Authorize(Policy = "ReadScopeClaim", ActiveAuthenticationSchemes = "Bearer")]
+    
+    [Authorize(Policy = "ReadScopeClaim", ActiveAuthenticationSchemes = "Bearer")]
     [ApiVersion("1.0")]
     [Route("api/users")]
     [Route("api/v{version:apiVersion}/users")]
