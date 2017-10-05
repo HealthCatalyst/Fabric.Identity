cdcf7044ab23107d5de2afabd055e9746fa313e7
michael.vidal <michael.vidal@healthcatalyst.com>
2017-10-05 17:19:46 -0400

    Added documentation to IdentityProvidersController. Added integration test for IdentityProvidersController.

---
+++ Fabric.Identity.IntegrationTests/IdentityProvidersControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Fabric.Identity.API.Models;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Fabric.Identity.IntegrationTests
+{
+    public class IdentityProvidersControllerTests : IntegrationTestsFixture
+    {
+        private readonly string _identityProvidersBaseUrl = "/api/identityProviders";
+        [Fact]
+        public async Task UsersController_Search_InvalidIdentityProvider_ReturnsBadRequest()
+        {
+            var response = await HttpClient.SendAsync(new HttpRequestMessage(new HttpMethod("GET"),
+                $"{_identityProvidersBaseUrl}"));
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            var externalProviders = JsonConvert.DeserializeObject<List<ExternalProviderApiModel>>(await response.Content.ReadAsStringAsync());
+            Assert.Equal(1, externalProviders.Count);
+
+        }
+    }
+}
