d435b91e87b7e4fa6433e1e4233576693dfc7159
Brian Smith <brian.smith@healthcatalyst.com>
2017-10-15 10:15:12 -0400

    set property defaults in API Client model to match IS4 Client model

--- Fabric.Identity.API/Models/Client.cs
+++ Fabric.Identity.API/Models/Client.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Security.Claims;
+using IdentityModel;
+using IdentityServer4;
 using IdentityServer4.Models;
 
 namespace Fabric.Identity.API.Models
@@ -10,46 +12,46 @@ namespace Fabric.Identity.API.Models
     public class Client
     {
         // Basic
-        public bool Enabled { get; set; }
+        public bool Enabled { get; set; } = true;
         public string ClientId { get; set; }
         public string ClientSecret { get; set; }
-        public bool RequireClientSecret { get; set; }
-        public IEnumerable<string> AllowedGrantTypes { get; set; }
-        public bool RequirePkce { get; set; }
-        public bool AllowPlainTextPkce { get; set; }
-        public ICollection<string> RedirectUris { get; set; }
-        public ICollection<string> AllowedScopes { get; set; }
-        public bool AllowOfflineAccess { get; set; }
-        public bool AllowAccessTokensViaBrowser { get; set; }
-        public string ProtocolType { get; set; }
+        public bool RequireClientSecret { get; set; } = true;
+        public IEnumerable<string> AllowedGrantTypes { get; set; } = GrantTypes.Implicit;
+        public bool RequirePkce { get; set; } = false;
+        public bool AllowPlainTextPkce { get; set; } = false;
+        public ICollection<string> RedirectUris { get; set; } = new HashSet<string>();
+        public ICollection<string> AllowedScopes { get; set; } = new HashSet<string>();
+        public bool AllowOfflineAccess { get; set; } = false;
+        public bool AllowAccessTokensViaBrowser { get; set; } = false;
+        public string ProtocolType { get; set; } = IdentityServerConstants.ProtocolTypes.OpenIdConnect;
 
         // Authentication/Logout
-        public ICollection<string> PostLogoutRedirectUris { get; set; }
-        public bool EnableLocalLogin { get; set; }
-        public ICollection<string> IdentityProviderRestrictions { get; set; }
+        public ICollection<string> PostLogoutRedirectUris { get; set; } = new HashSet<string>();
+        public bool EnableLocalLogin { get; set; } = true;
+        public ICollection<string> IdentityProviderRestrictions { get; set; } = new HashSet<string>();
         public string LogoutUri { get; set; }
-        public bool LogoutSessionRequired { get; set; }
+        public bool LogoutSessionRequired { get; set; } = true;
 
         // Token
-        public int IdentityTokenLifetime { get; set; }
-        public int AccessTokenLifetime { get; set; }
-        public int AuthorizationCodeLifetime { get; set; }
-        public int AbsoluteRefreshTokenLifetime { get; set; }
-        public int SlidingRefreshTokenLifetime { get; set; }
-        public TokenUsage RefreshTokenUsage { get; set; }
-        public TokenExpiration RefreshTokenExpiration { get; set; }
-        public bool UpdateAccessTokenClaimsOnRefresh { get; set; }
-        public AccessTokenType AccessTokenType { get; set; }
-        public bool IncludeJwtId { get; set; }
-        public ICollection<string> AllowedCorsOrigins { get; set; }
-        public ICollection<Claim> Claims { get; set; }
-        public bool AlwaysSendClientClaims { get; set; }
-        public bool AlwaysIncludeUserClaimsInIdToken { get; set; }
-        public bool PrefixClientClaims { get; set; }
+        public int IdentityTokenLifetime { get; set; } = 300;
+        public int AccessTokenLifetime { get; set; } = 3600;
+        public int AuthorizationCodeLifetime { get; set; } = 300;
+        public int AbsoluteRefreshTokenLifetime { get; set; } = 2592000;
+        public int SlidingRefreshTokenLifetime { get; set; } = 1296000;
+        public TokenUsage RefreshTokenUsage { get; set; } = TokenUsage.OneTimeOnly;
+        public TokenExpiration RefreshTokenExpiration { get; set; } = TokenExpiration.Absolute;
+        public bool UpdateAccessTokenClaimsOnRefresh { get; set; } = false;
+        public AccessTokenType AccessTokenType { get; set; } = AccessTokenType.Jwt;
+        public bool IncludeJwtId { get; set; } = false;
+        public ICollection<string> AllowedCorsOrigins { get; set; } = new HashSet<string>();
+        public ICollection<Claim> Claims { get; set; } = new HashSet<Claim>(new ClaimComparer());
+        public bool AlwaysSendClientClaims { get; set; } = false;
+        public bool AlwaysIncludeUserClaimsInIdToken { get; set; } = false;
+        public bool PrefixClientClaims { get; set; } = true;
 
         // Consent fields
-        public bool RequireConsent { get; set; }
-        public bool AllowRememberConsent { get; set; }
+        public bool RequireConsent { get; set; } = true;
+        public bool AllowRememberConsent { get; set; } = true;
         public string ClientName { get; set; }
         public string ClientUri { get; set; }
         public string LogoUri { get; set; }
