11a7c2a6fcac57e7950e73ffa5f164e8c393ba00
Brian Smith <brian.smith@healthcatalyst.com>
2017-11-01 15:27:54 -0400

    1) added in memory resource stores and 2) added I*ResourceStore registrations

---
+++ Fabric.Identity.API/Persistence/InMemory/Stores/InMemoryIdentityResourceStore.cs
@@ -0,0 +1,32 @@
+﻿using Fabric.Identity.API.Persistence.CouchDb.Stores;
+using IdentityServer4.Models;
+
+namespace Fabric.Identity.API.Persistence.InMemory.Stores
+{
+    public class InMemoryIdentityResourceStore : BaseResourceStore, IIdentityResourceStore
+    {
+        public InMemoryIdentityResourceStore(IDocumentDbService documentDbService) : base(documentDbService)
+        {
+        }
+
+        public IdentityResource GetResource(string id)
+        {
+            return DocumentDbService.GetDocument<IdentityResource>(id).Result;
+        }
+
+        public void AddResource(IdentityResource apiResource)
+        {
+            DocumentDbService.AddDocument(apiResource.Name, apiResource);
+        }
+
+        public void UpdateResource(string id, IdentityResource apiResource)
+        {
+            DocumentDbService.UpdateDocument(id, apiResource);
+        }
+
+        public void DeleteResource(string id)
+        {
+            DocumentDbService.DeleteDocument<IdentityResource>(id);
+        }
+    }
+}
