a622d8d9bf822f98d5ef5840d7a5f9e4fbf37648
Brian Smith <brian.smith@healthcatalyst.com>
2017-11-02 15:10:33 -0400

    added base resource class for common *Resource model properties

---
+++ Fabric.Identity.API/Models/BaseResource.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Fabric.Identity.API.Models
+{
+    public abstract class BaseResource
+    {
+        /// <summary>
+        ///     Indicates if this resource is enabled. Defaults to true.
+        /// </summary>
+        public bool Enabled { get; set; } = true;
+
+        /// <summary>
+        ///     The unique name of the resource.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        ///     Display name of the resource.
+        /// </summary>
+        public string DisplayName { get; set; }
+
+        /// <summary>
+        ///     Description of the resource.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        ///     List of accociated user claims that should be included when this resource is requested.
+        /// </summary>
+        public ICollection<string> UserClaims { get; set; } = new HashSet<string>();
+    }
+}
