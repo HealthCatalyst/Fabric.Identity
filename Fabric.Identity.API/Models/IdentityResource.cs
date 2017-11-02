3a93dbdac31402909b09bee30297dac8c1e55d1f
Brian Smith <brian.smith@healthcatalyst.com>
2017-11-02 15:01:24 -0400

    default API view model properties to match IS4 library defaults

--- Fabric.Identity.API/Models/IdentityResource.cs
+++ Fabric.Identity.API/Models/IdentityResource.cs
@@ -4,13 +4,13 @@ namespace Fabric.Identity.API.Models
 {
     public class IdentityResource
     {
-        public bool Enabled { get; set; }
+        public bool Enabled { get; set; } = true;
         public string Name { get; set; }
         public string DisplayName { get; set; }
         public string Description { get; set; }
         public bool Required { get; set; }
         public bool Emphasize { get; set; }
-        public bool ShowInDiscoveryDocument { get; set; }
-        public ICollection<string> UserClaims { get; set; }
+        public bool ShowInDiscoveryDocument { get; set; } = true;
+        public ICollection<string> UserClaims { get; set; } = new HashSet<string>();
     }
 }
