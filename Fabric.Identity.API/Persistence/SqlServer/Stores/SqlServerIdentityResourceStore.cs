43f44b627b8cf18024659cc02ca55d48f8602866
Kyle Paul <djjoebanks@gmail.com>
2017-11-02 17:55:11 -0700

    implemented sql server identity resource store and identity server resource store

---
+++ Fabric.Identity.API/Persistence/SqlServer/Stores/SqlServerIdentityResourceStore.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Fabric.Identity.API.Persistence.SqlServer.Models;
+using Fabric.Identity.API.Persistence.SqlServer.Services;
+using IdentityServer4.EntityFramework.Mappers;
+using IdentityServer4.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fabric.Identity.API.Persistence.SqlServer.Stores
+{
+    public class SqlServerIdentityResourceStore : SqlServerResourceStore, IIdentityResourceStore
+    {
+        public SqlServerIdentityResourceStore(IIdentityDbContext identityDbContext) :
+            base(identityDbContext)
+        {
+            
+        }
+
+        public void AddResource(IdentityResource resource)
+        {
+            AddResourceAsync(resource).Wait();
+        }
+
+        public void UpdateResource(string id, IdentityResource resource)
+        {
+            UpdateResourceAsync(id, resource).Wait();
+        }
+
+        public IdentityResource GetResource(string id)
+        {
+            return GetResourceAsync(id).Result;
+        }
+
+        public void DeleteResource(string id)
+        {
+            DeleteResourceAsync(id).Wait();
+        }
+
+        public async Task AddResourceAsync(IdentityResource resource)
+        {
+            var resourceEntity = resource.ToDomainModel();
+
+            //TODO: set entity properties
+
+            await IdentityDbContext.IdentityResources.AddAsync(resourceEntity);
+        }
+
+        public async Task UpdateResourceAsync(string id, IdentityResource resource)
+        {
+            var identityResourceEntity = resource.ToDomainModel();
+
+            //TODO: set entity properties
+
+            IdentityDbContext.IdentityResources.Update(identityResourceEntity);
+            await IdentityDbContext.SaveChangesAsync();
+        }
+
+        public async Task<IdentityResource> GetResourceAsync(string id)
+        {
+            var identityResourceEntity = await IdentityDbContext.IdentityResources
+                .FirstOrDefaultAsync(i => i.Name.Equals(id, StringComparison.CurrentCultureIgnoreCase));
+
+            return identityResourceEntity?.ToModel();
+        }
+
+        public async Task DeleteResourceAsync(string id)
+        {
+            var identityResourceToDelete =
+                await IdentityDbContext.IdentityResources.FirstOrDefaultAsync(a =>
+                    a.Name.Equals(id, StringComparison.OrdinalIgnoreCase));
+
+            //TODO: set other domain model properties
+
+            identityResourceToDelete.IsDeleted = true;
+
+            await IdentityDbContext.SaveChangesAsync();
+        }
+    }
+}
