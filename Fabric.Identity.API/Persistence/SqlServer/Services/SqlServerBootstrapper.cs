178497369e267376530457c3bffa1d047eda0728
Brian Smith <brian.smith@healthcatalyst.com>
2017-11-08 16:22:00 -0500

    initial work for wiring up to Sql Server

--- Fabric.Identity.API/Persistence/SqlServer/Services/SqlServerBootstrapper.cs
+++ Fabric.Identity.API/Persistence/SqlServer/Services/SqlServerBootstrapper.cs
@@ -1,18 +1,42 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using Fabric.Identity.API.Persistence.SqlServer.Mappers;
 using IdentityServer4.Models;
+using Serilog;
 
 namespace Fabric.Identity.API.Persistence.SqlServer.Services
 {
     public class SqlServerBootstrapper : IDbBootstrapper
     {
+        private readonly IIdentityDbContext _identityDbContext;
+        private readonly ILogger _logger;
+
+        public SqlServerBootstrapper(IIdentityDbContext identityDbContext, ILogger logger)
+        {
+            _identityDbContext = identityDbContext;
+            _logger = logger;
+        }
+
         public bool Setup()
         {
             return true;
         }
 
-        public void AddResources(IEnumerable<IdentityResource> resources)
+        public async void AddResources(IEnumerable<IdentityResource> resources)
         {
-            
+            foreach (var identityResource in resources)
+            {
+                try
+                {
+                    _identityDbContext.IdentityResources.Add(identityResource.ToEntity());
+                }
+                catch (Exception ex)
+                {
+                    _logger.Warning(ex, ex.Message);
+                }
+            }
+
+            await _identityDbContext.SaveChangesAsync();
         }
     }
 }
