080729283b96135943c861749e71d34e1c28223f
Kyle Paul <djjoebanks@gmail.com>
2017-11-08 13:45:13 -0800

    added  integration test classes for sql server

---
+++ Fabric.Identity.IntegrationTests/ControllerTests/SqlServer/SqlServerClientRegistrationTests.cs
@@ -0,0 +1,12 @@
+﻿using Fabric.Identity.API;
+
+namespace Fabric.Identity.IntegrationTests.ControllerTests.SqlServer
+{
+    public class SqlServerClientRegistrationTests : InMemory.ClientRegistrationTests
+    {
+        public SqlServerClientRegistrationTests(string storageProvider = FabricIdentityConstants.StorageProviders.SqlServer)
+            : base(storageProvider)
+        {
+        }
+    }
+}
