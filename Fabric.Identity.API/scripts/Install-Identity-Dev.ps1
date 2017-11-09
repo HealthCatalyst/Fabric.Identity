9646f50f4fb90e01402fbba6bec10d7b1b15657c
michael.vidal <michael.vidal@healthcatalyst.com>
2017-11-08 22:02:00 -0500

    Update dev install script to install CouchDB and windows server hosting components if not present.

--- Fabric.Identity.API/scripts/Install-Identity-Dev.ps1
+++ Fabric.Identity.API/scripts/Install-Identity-Dev.ps1
@@ -1,8 +1,8 @@
 #
 # Install_Identity_Dev.ps1
 #	
-param([String] $couchDbUsername,
-	  [String] $couchDbPassword) 
+param([String] $couchDbUsername = "admin",
+	  [String] $couchDbPassword = "admin") 
 
 function Test-RegistrationComplete($authUrl)
 {
@@ -22,6 +22,30 @@ function Test-RegistrationComplete($authUrl)
     }
 
     return $false
+}
+
+function Create-CouchDb($couchDbUsername, $couchDbPassword, $couchDbServer)
+{
+	try	{
+		Write-Host "Installing CouchDB via docker..."
+		docker stop fabric.couchdb
+		docker rm fabric.couchdb
+		docker volume rm couchdb-data
+		docker run -d --name fabric.couchdb -e "COUCHDB_USER=$couchDbUsername" -e "COUCHDB_PASSWORD=$couchDbPassword" -v couchdb-data:/opt/couchdb/data -p 0.0.0.0:5984:5984 healthcatalyst/fabric.docker.couchdb
+	} catch [System.Management.Automation.CommandNotFoundException]{
+		Write-Host "Docker not installed, downloading and installing CouchDB for Windows..."
+		Invoke-WebRequest -Uri https://dl.bintray.com/apache/couchdb/win/2.1.0/apache-couchdb-2.1.0.msi -OutFile $env:Temp\apache-couchdb-2.1.0.msi
+        Write-Host "Launching CouchDB interactive installation..."
+        Start-Process $env:Temp\apache-couchdb-2.1.0.msi -Wait
+        Remove-Item $env:Temp\apache-couchdb-2.1.0.msi
+        try{
+            Invoke-RestMethod -Method Put -Uri "$couchDbServer/_node/couchdb@localhost/_config/admins/$couchDbUsername" -Body "`"$couchDbPassword`""
+        } catch{
+            $exception = $_.Exception
+            Write-Host "Failed to create admin user for CouchDB. Exception $exception"
+			throw
+        }
+	}
 }
 
 if(!(Test-Path .\Fabric-Install-Utilities.psm1)){
@@ -51,8 +75,12 @@ $workingDirectory = Get-CurrentScriptDirectory
 
 if(!(Test-Prerequisite '*.NET Core*Windows Server Hosting*' 1.1.30327.81))
 {
-    Write-Host ".NET Core Windows Server Hosting Bundle minimum version 1.1.30327.81 not installed...download and install from https://go.microsoft.com/fwlink/?linkid=844461. Halting installation."
-    exit 1
+     Write-Host "Windows Server Hosting Bundle minimum version 1.1.30327.81 not installed...installing version 1.1.30327.81"
+     Invoke-WebRequest -Uri https://go.microsoft.com/fwlink/?linkid=844461 -OutFile $env:Temp\bundle.exe
+     Start-Process $env:Temp\bundle.exe -Wait -ArgumentList '/quiet /install'
+     net stop was /y
+     net start w3svc
+     Remove-Item $env:Temp\bundle.exe
 }else{
     Write-Host ".NET Core Windows Server Hosting Bundle installed and meets expectations."
 }
@@ -63,8 +91,13 @@ if(!(Test-Prerequisite '*CouchDB*'))
     $remoteInstallationStatus = Get-CouchDbRemoteInstallationStatus $couchDbServer 2.0.0
     if($remoteInstallationStatus -eq "NotInstalled")
     {
-        Write-Host "CouchDB not installed, download and install from https://dl.bintray.com/apache/couchdb/win/2.1.0/apache-couchdb-2.1.0.msi. Halting installation."
-		exit 1
+        Write-Host "CouchDB not installed, attempting to install couchdb..."
+		try{
+			Create-CouchDb $couchDbUsername $couchDbPassword $couchDbServer
+		} catch{
+			$exception = $_.Exception
+            Write-Error "Failed to create CouchDB. Exception $exception" -ErrorAction Stop
+		}
     }elseif($remoteInstallationStatus -eq "MinVersionNotMet"){
         Write-Host "CouchDB is installed on $couchDbServer but does not meet the minimum version requirements, you must have CouchDB 2.0.0.1 or greater installed: https://dl.bintray.com/apache/couchdb/win/2.1.0/apache-couchdb-2.1.0.msi. Halting installation."
         exit 1
@@ -88,7 +121,8 @@ try{
 }catch [System.InvalidOperationException]{
 	Write-Host "AppPool $appName is already stopped, continuing."
 }
-
+dotnet restore ..\Fabric.Identity.API.csproj
+dotnet build ..\Fabric.Identity.API.csproj
 dotnet publish ..\Fabric.Identity.API.csproj -c Debug -o $appDirectory
 
 Start-WebAppPool -Name $appName
@@ -96,7 +130,7 @@ Start-WebAppPool -Name $appName
 
 #Write environment variables
 Write-Host "Loading up environment variables..."
-$environmentVariables = @{"HostingOptions__UseInMemoryStores" = "false"; "HostingOptions__UseTestUsers" = "false"; "AllowLocalLogin" = "false"}
+$environmentVariables = @{"HostingOptions__StorageProvider" = "CouchDb"; "HostingOptions__UseTestUsers" = "false"; "AllowLocalLogin" = "false"}
 
 
 if ($couchDbUsername){
@@ -135,23 +169,67 @@ $registrationApiSecret = Add-ApiRegistration -authUrl $identityServerUrl -body $
 #Register Fabric.Installer
 $body = @'
 {
-    "clientId":"fabric-installer", 
-    "clientName":"Fabric Installer", 
-    "requireConsent":"false", 
-    "allowedGrantTypes": ["client_credentials"], 
-    "allowedScopes": ["fabric/identity.manageresources", "fabric/authorization.read", "fabric/authorization.write", "fabric/authorization.manageclients"]
+  "Enabled": true,
+  "ClientId": "fabric-installer",
+  "ProtocolType": "oidc",
+  "ClientSecrets": [
+    {
+      "Description": null,
+      "Value": "xUNCqCJTNeM3NDd3fH0LGIOftRHrcaHB3DwRQACZdYA=",
+      "Expiration": null,
+      "Type": "SharedSecret"
+    }
+  ],
+  "RequireClientSecret": true,
+  "ClientName": "Fabric Installer",
+  "ClientUri": null,
+  "LogoUri": null,
+  "RequireConsent": false,
+  "AllowRememberConsent": true,
+  "AllowedGrantTypes": [
+    "client_credentials"
+  ],
+  "RequirePkce": false,
+  "AllowPlainTextPkce": false,
+  "AllowAccessTokensViaBrowser": false,
+  "RedirectUris": [],
+  "PostLogoutRedirectUris": [],
+  "LogoutUri": null,
+  "LogoutSessionRequired": true,
+  "AllowOfflineAccess": false,
+  "AllowedScopes": [
+    "fabric/identity.manageresources",
+    "fabric/authorization.read",
+    "fabric/authorization.write",
+    "fabric/authorization.manageclients"
+  ],
+  "AlwaysIncludeUserClaimsInIdToken": false,
+  "IdentityTokenLifetime": 300,
+  "AccessTokenLifetime": 3600,
+  "AuthorizationCodeLifetime": 300,
+  "AbsoluteRefreshTokenLifetime": 2592000,
+  "SlidingRefreshTokenLifetime": 1296000,
+  "RefreshTokenUsage": 1,
+  "UpdateAccessTokenClaimsOnRefresh": false,
+  "RefreshTokenExpiration": 1,
+  "AccessTokenType": 0,
+  "EnableLocalLogin": true,
+  "IdentityProviderRestrictions": [],
+  "IncludeJwtId": false,
+  "Claims": [],
+  "AlwaysSendClientClaims": false,
+  "PrefixClientClaims": true,
+  "AllowedCorsOrigins": []
 }
 '@
 
-Write-Host "Registering Fabric.Installer."
-$installerClientSecret = Add-ClientRegistration -authUrl $identityServerUrl -body $body
+$headers = @{"Accept" = "application/json"}
+$credentials = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes("$($couchDbUserName):$($couchDbPassword)"))
+$headers.Add("Authorization", "Basic $credentials")
 
-Write-Host ""
-Write-Host "Please keep the following secrets in a secure place:"
-Write-Host "Fabric.Installer clientSecret: $installerClientSecret"
-Write-Host "Fabric.Registration apiSecret: $registrationApiSecret"
-Write-Host ""
-Write-Host "The Fabric.Installer clientSecret will be needed in subsequent installations:"
-Write-Host "Fabric.Installer clientSecret: $installerClientSecret"
+Write-Host "Registering Fabric.Installer."
+Write-Host $headers
+Write-Host "Client URL: $couchDbServer"
+$response = Invoke-RestMethod -Method Put -Uri "$couchDbServer/identity/client%3Afabric-installer" -Body $body -ContentType "application/json" -Headers $headers
 
 Write-Host "Installation complete, exiting."
