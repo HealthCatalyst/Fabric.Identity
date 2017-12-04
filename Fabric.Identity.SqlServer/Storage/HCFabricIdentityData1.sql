28d86ef738df409439ed7b1bd1ffcad1eadc7de2
Brian Smith <brian.smith@healthcatalyst.com>
2017-12-04 12:40:59 -0500

    1) added index and primary files for Fabric Identity Sql Server instance, 2) split MountPoints script into separate scripts to match existing conventions

---
+++ Fabric.Identity.SqlServer/Storage/HCFabricIdentityData1.sql
@@ -0,0 +1,22 @@
+﻿/*
+Do not change the database path or name variables.
+Any sqlcmd variables will be properly substituted during 
+build and deployment.
+*/
+
+ALTER DATABASE [$(DatabaseName)]
+	ADD FILEGROUP [HCFabricIdentityData1] 
+
+	GO
+ALTER DATABASE [$(DatabaseName)]
+	ADD FILE
+	(
+		NAME = [HCFabricIdentityData1File1],
+		FILENAME = '$(FabricIdentityDataMountPoint)\HC$(DatabaseName)Data1File1.ndf',
+		SIZE = 100MB,
+		MAXSIZE = 5GB,
+		FILEGROWTH = 100MB
+	)
+
+TO FILEGROUP [HCFabricIdentityData1];
+GO
