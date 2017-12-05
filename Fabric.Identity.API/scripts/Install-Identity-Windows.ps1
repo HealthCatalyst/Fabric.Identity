76a60c1591f5688295d4a73c41943818ba3d4d6b
michael.vidal <michael.vidal@healthcatalyst.com>
2017-12-05 07:29:53 -0500

    Force user to enter a service account and remember it.

--- Fabric.Identity.API/scripts/Install-Identity-Windows.ps1
+++ Fabric.Identity.API/scripts/Install-Identity-Windows.ps1
@@ -58,8 +58,8 @@ function Add-DatabaseUserToRole($userName, $connString, $role)
 }
 
 function Add-ServiceUserToDiscovery($userName, $connString){
-	$query = "DECLARE @IdentityID int;
-				DECLARE @DiscoveryServiceUserRoleID int;
+    $query = "DECLARE @IdentityID int;
+                DECLARE @DiscoveryServiceUserRoleID int;
 
 				SELECT @IdentityID = IdentityID FROM CatalystAdmin.IdentityBASE WHERE IdentityNM = @userName;
 				IF (@IdentityID IS NULL)
@@ -88,35 +88,35 @@ function Add-DatabaseSecurity($userName, $role, $connString)
 
 function Add-DiscoveryRegistration($discoveryUrl, $serviceUrl, $credential)
 {
-	$registrationBody = @{
-		ServiceName = "IdentityService"
-		Version = 1
-		ServiceUrl = $serviceUrl
-		DiscoveryType = "Service"
-		IsHidden = $true
-		FriendlyName = "Fabric.Identity"
-		Description = "The Fabric.Identity service provides centralized authentication across the Fabric ecosystem."
-		BuildNumber = "1.1.2017120101"
-	}
+    $registrationBody = @{
+        ServiceName = "IdentityService"
+        Version = 1
+        ServiceUrl = $serviceUrl
+        DiscoveryType = "Service"
+        IsHidden = $true
+        FriendlyName = "Fabric.Identity"
+        Description = "The Fabric.Identity service provides centralized authentication across the Fabric ecosystem."
+        BuildNumber = "1.1.2017120101"
+    }
 
-	$url = "$discoveryUrl/v1/Services"
-	$jsonBody = $registrationBody | ConvertTo-Json
-	try{
-		Invoke-RestMethod -Method Post -Uri "$url" -Body "$jsonBody" -ContentType "application/json" -Credential $credential | Out-Null
-		Write-Success "Fabric.Identity successfully registered with DiscoveryService."
-	}catch{
-		Write-Error "Unable to register Fabric.Identity with DiscoveryService. Error $($_.Exception.Message) Halting installation." -ErrorAction Stop
-	}
+    $url = "$discoveryUrl/v1/Services"
+    $jsonBody = $registrationBody | ConvertTo-Json
+    try{
+        Invoke-RestMethod -Method Post -Uri "$url" -Body "$jsonBody" -ContentType "application/json" -Credential $credential | Out-Null
+        Write-Success "Fabric.Identity successfully registered with DiscoveryService."
+    }catch{
+        Write-Error "Unable to register Fabric.Identity with DiscoveryService. Error $($_.Exception.Message) Halting installation." -ErrorAction Stop
+    }
 }
 
 if(!(Test-Path .\Fabric-Install-Utilities.psm1)){
-	Invoke-WebRequest -Uri https://raw.githubusercontent.com/HealthCatalyst/InstallScripts/master/common/Fabric-Install-Utilities.psm1 -Headers @{"Cache-Control"="no-cache"} -OutFile Fabric-Install-Utilities.psm1
+    Invoke-WebRequest -Uri https://raw.githubusercontent.com/HealthCatalyst/InstallScripts/master/common/Fabric-Install-Utilities.psm1 -Headers @{"Cache-Control"="no-cache"} -OutFile Fabric-Install-Utilities.psm1
 }
 Import-Module -Name .\Fabric-Install-Utilities.psm1 -Force
 
 if(!(Test-IsRunAsAdministrator))
 {
-	Write-Error "You must run this script as an administrator. Halting configuration." -ErrorAction Stop
+    Write-Error "You must run this script as an administrator. Halting configuration." -ErrorAction Stop
 }
 
 function Unlock-ConfigurationSections(){   
@@ -154,34 +154,34 @@ function Invoke-Sql($connectionString, $sql, $parameters=@{}){
 
 function Get-ApplicationEndpoint($appName)
 {
-	return "http://$env:computername/$appName"
+    return "http://$env:computername/$appName"
 }
 
 function Get-DiscoveryServiceUrl()
 {
-	return "http://$env:computername/DiscoveryService"
+    return "http://$env:computername/DiscoveryService"
 }
 
 function Add-PermissionToPrivateKey($iisUser, $signingCert, $permission){
-	try{
+    try{
         $allowRule = New-Object security.accesscontrol.filesystemaccessrule $iisUser, $permission, allow
         $keyFolder = "c:\programdata\microsoft\crypto\rsa\machinekeys"    
 
 		$keyname = $signingCert.privatekey.cspkeycontainerinfo.uniquekeycontainername        
 		$keyPath = [io.path]::combine($keyFolder, $keyname)		
 
-		if ([io.file]::exists($keyPath))
-		{        
-			$acl = Get-Acl $keyPath
-			$acl.AddAccessRule($allowRule)
-			Set-Acl $keyPath $acl			
-			Write-Success "The permission '$($permission)' was successfully added to the private key for user '$($iisUser)'"
-		}else{
-			Write-Error "No key file was found at '$($keyPath)'. Ensure a valid signing certificate was provided" -ErrorAction Stop
-		}
-	}catch{
-		Write-Error "There was an error adding the '$($permission)' permission for the user '$($iisUser)' to the private key. Error $($_.Exception.Message)" -ErrorAction Stop
-	}	
+        if ([io.file]::exists($keyPath))
+        {        
+            $acl = Get-Acl $keyPath
+            $acl.AddAccessRule($allowRule)
+            Set-Acl $keyPath $acl			
+            Write-Success "The permission '$($permission)' was successfully added to the private key for user '$($iisUser)'"
+        }else{
+            Write-Error "No key file was found at '$($keyPath)'. Ensure a valid signing certificate was provided" -ErrorAction Stop
+        }
+    }catch{
+        Write-Error "There was an error adding the '$($permission)' permission for the user '$($iisUser)' to the private key. Error $($_.Exception.Message)" -ErrorAction Stop
+    }	
 }
 
 $installSettings = Get-InstallationSettings "identity"
@@ -197,35 +197,35 @@ $identityDbName = $installSettings.identityDbName
 $identityDatabaseRole = $installSettings.identityDatabaseRole
 $discoveryServiceUrl = Get-DiscoveryServiceUrl
 $applicationEndPoint = Get-ApplicationEndpoint $appName
-$iisUser = "IIS AppPool\$appName"
+$storedIisUser = $installSettings.iisUser
 $useSpecificUser = $false;
 
 $workingDirectory = Get-CurrentScriptDirectory
 
 try{
-	$sites = Get-ChildItem IIS:\Sites
-	if($sites -is [array]){
-		$sites |
-			ForEach-Object {New-Object PSCustomObject -Property @{
-				'Id'=$_.id;
-				'Name'=$_.name;
-				'Physical Path'=[System.Environment]::ExpandEnvironmentVariables($_.physicalPath);
-				'Bindings'=$_.bindings;
-			};} |
-			Format-Table Id,Name,'Physical Path',Bindings -AutoSize
+    $sites = Get-ChildItem IIS:\Sites
+    if($sites -is [array]){
+        $sites |
+            ForEach-Object {New-Object PSCustomObject -Property @{
+                'Id'=$_.id;
+                'Name'=$_.name;
+                'Physical Path'=[System.Environment]::ExpandEnvironmentVariables($_.physicalPath);
+                'Bindings'=$_.bindings;
+            };} |
+            Format-Table Id,Name,'Physical Path',Bindings -AutoSize
 
-		$selectedSiteId = Read-Host "Select a web site by Id"
+        $selectedSiteId = Read-Host "Select a web site by Id"
 
-		$selectedSite = $sites[$selectedSiteId - 1]
-	}else{
-		$selectedSite = $sites
-	}
+        $selectedSite = $sites[$selectedSiteId - 1]
+    }else{
+        $selectedSite = $sites
+    }
 
     $webroot = [System.Environment]::ExpandEnvironmentVariables($selectedSite.physicalPath)    
-	$siteName = $selectedSite.name
+    $siteName = $selectedSite.name
 
 }catch{
-	Write-Error "Could not select a website." -ErrorAction Stop
+    Write-Error "Could not select a website." -ErrorAction Stop
 }
 
 try{
@@ -253,43 +253,43 @@ try{
 
 
 try{
-	$signingCert = Get-Certificate $primarySigningCertificateThumbprint
+    $signingCert = Get-Certificate $primarySigningCertificateThumbprint
 }catch{
-	Write-Error "Could not get signing certificate with thumbprint $primarySigningCertificateThumbprint. Please verify that the primarySigningCertificateThumbprint setting in install.config contains a valid thumbprint for a certificate in the Local Machine Personal store."
-	throw $_.Exception
+    Write-Error "Could not get signing certificate with thumbprint $primarySigningCertificateThumbprint. Please verify that the primarySigningCertificateThumbprint setting in install.config contains a valid thumbprint for a certificate in the Local Machine Personal store."
+    throw $_.Exception
 }
 
 try{
-	$encryptionCert = Get-Certificate $encryptionCertificateThumbprint
+    $encryptionCert = Get-Certificate $encryptionCertificateThumbprint
 }catch{
-	Write-Error "Could not get encryption certificate with thumbprint $encryptionCertificateThumbprint. Please verify that the encryptionCertificateThumbprint setting in install.config contains a valid thumbprint for a certificate in the Local Machine Personal store."
-	throw $_.Exception
+    Write-Error "Could not get encryption certificate with thumbprint $encryptionCertificateThumbprint. Please verify that the encryptionCertificateThumbprint setting in install.config contains a valid thumbprint for a certificate in the Local Machine Personal store."
+    throw $_.Exception
 }
 
 if((Test-Path $zipPackage))
 {
-	$path = [System.IO.Path]::GetDirectoryName($zipPackage)
-	if(!$path)
-	{
-		$zipPackage = [System.IO.Path]::Combine($workingDirectory, $zipPackage)
-		Write-Console "zipPackage: $zipPackage"
-	}
+    $path = [System.IO.Path]::GetDirectoryName($zipPackage)
+    if(!$path)
+    {
+        $zipPackage = [System.IO.Path]::Combine($workingDirectory, $zipPackage)
+        Write-Console "zipPackage: $zipPackage"
+    }
 }else{
-	Write-Error "Could not find file or directory $zipPackage, please verify that the zipPackage configuration setting in install.config is the path to a valid zip file that exists." -ErrorAction Stop	
+    Write-Error "Could not find file or directory $zipPackage, please verify that the zipPackage configuration setting in install.config is the path to a valid zip file that exists." -ErrorAction Stop	
 }
 
 
 if(!(Test-PrerequisiteExact "*.NET Core*Windows Server Hosting*" 1.1.30327.81))
 {    
     try{
-		Write-Console "Windows Server Hosting Bundle version 1.1.30327.81 not installed...installing version 1.1.30327.81"        
-		Invoke-WebRequest -Uri https://go.microsoft.com/fwlink/?linkid=844461 -OutFile $env:Temp\bundle.exe
-		Start-Process $env:Temp\bundle.exe -Wait -ArgumentList '/quiet /install'
-		net stop was /y
-		net start w3svc			
-	}catch{
-	    Write-Error "Could not install .NET Windows Server Hosting bundle. Please install the hosting bundle before proceeding. https://go.microsoft.com/fwlink/?linkid=844461" -ErrorAction Stop
-	}
+        Write-Console "Windows Server Hosting Bundle version 1.1.30327.81 not installed...installing version 1.1.30327.81"        
+        Invoke-WebRequest -Uri https://go.microsoft.com/fwlink/?linkid=844461 -OutFile $env:Temp\bundle.exe
+        Start-Process $env:Temp\bundle.exe -Wait -ArgumentList '/quiet /install'
+        net stop was /y
+        net start w3svc			
+    }catch{
+        Write-Error "Could not install .NET Windows Server Hosting bundle. Please install the hosting bundle before proceeding. https://go.microsoft.com/fwlink/?linkid=844461" -ErrorAction Stop
+    }
     try{
         Remove-Item $env:Temp\bundle.exe
     }catch{        
@@ -311,12 +311,12 @@ if(![string]::IsNullOrEmpty($userEnteredAppInsightsInstrumentationKey)){
 $userEnteredSqlServerAddress = Read-Host "Press Enter to accept the default Sql Server address '$($sqlServerAddress)' or enter a new Sql Server address" 
 
 if(![string]::IsNullOrEmpty($userEnteredSqlServerAddress)){
-	$sqlServerAddress = $userEnteredSqlServerAddress
+    $sqlServerAddress = $userEnteredSqlServerAddress
 }
 
 $userEnteredMetadataDbName = Read-Host "Press Enter to accept the default Metadata DB Name '$($metadataDbName)' or enter a new Metadata DB Name"
 if(![string]::IsNullOrEmpty($userEnteredMetadataDbName)){
-	$metadataDbName = $userEnteredMetadataDbName
+    $metadataDbName = $userEnteredMetadataDbName
 }
 
 $metadataConnStr = "Server=$($sqlServerAddress);Database=$($metadataDbName);Trusted_Connection=True;MultipleActiveResultSets=True;"
@@ -326,7 +326,7 @@ Write-Success "Metadata DB Connection string: $metadataConnStr verified"
 
 $userEnteredIdentityDbName = Read-Host "Press Enter to accept the default Identity DB Name '$($identityDbName)' or enter a new Identity DB Name"
 if(![string]::IsNullOrEmpty($userEnteredIdentityDbName)){
-	$identityDbName = $userEnteredIdentityDbName
+    $identityDbName = $userEnteredIdentityDbName
 }
 
 $identityDbConnStr = "Server=$($sqlServerAddress);Database=$($identityDbName);Trusted_Connection=True;MultipleActiveResultSets=True;"
@@ -335,22 +335,31 @@ Invoke-Sql $identityDbConnStr "SELECT TOP 1 ClientId FROM Clients" | Out-Null
 Write-Success "Identity DB Connection string: $identityDbConnStr verified"
 
 
-
-$userEnteredIisUser = Read-Host "Press Enter to accept the default IIS App Pool User '$($iisUser)' or enter a new App Pool User"
+if(![string]::IsNullOrEmpty($storedIisUser)){
+    $userEnteredIisUser = Read-Host "Press Enter to accept the default IIS App Pool User '$($storedIisUser)' or enter a new App Pool User"
+    if([string]::IsNullOrEmpty($userEnteredIisUser)){
+        $userEnteredIisUser = $storedIisUser
+    }
+}else{
+    $userEnteredIisUser = Read-Host "Please enter a user account for the App Pool"
+}
 
 if(![string]::IsNullOrEmpty($userEnteredIisUser)){
-	$iisUser = $userEnteredIisUser
-	$useSpecificUser = $true
-	$userEnteredPassword = Read-Host "Enter the password for $iisUser" -AsSecureString
-	$credential = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList $iisUser, $userEnteredPassword
-	[System.Reflection.Assembly]::LoadWithPartialName("System.DirectoryServices.AccountManagement")
-	$ct = [System.DirectoryServices.AccountManagement.ContextType]::Domain
-	$pc = New-Object System.DirectoryServices.AccountManagement.PrincipalContext -ArgumentList $ct,$credential.GetNetworkCredential().Domain
-	$isValid = $pc.ValidateCredentials($credential.GetNetworkCredential().UserName, $credential.GetNetworkCredential().Password)
-	if(!$isValid){
-		Write-Error "Incorrect credentials for $iisUser" -ErrorAction Stop
-	}
-	Write-Success "Credentials are valid for user $iisUser"
+    
+    $iisUser = $userEnteredIisUser
+    $useSpecificUser = $true
+    $userEnteredPassword = Read-Host "Enter the password for $iisUser" -AsSecureString
+    $credential = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList $iisUser, $userEnteredPassword
+    [System.Reflection.Assembly]::LoadWithPartialName("System.DirectoryServices.AccountManagement")
+    $ct = [System.DirectoryServices.AccountManagement.ContextType]::Domain
+    $pc = New-Object System.DirectoryServices.AccountManagement.PrincipalContext -ArgumentList $ct,$credential.GetNetworkCredential().Domain
+    $isValid = $pc.ValidateCredentials($credential.GetNetworkCredential().UserName, $credential.GetNetworkCredential().Password)
+    if(!$isValid){
+        Write-Error "Incorrect credentials for $iisUser" -ErrorAction Stop
+    }
+    Write-Success "Credentials are valid for user $iisUser"
+}else{
+    Write-Error "No user account was entered, please enter a valid user account." -ErrorAction Stop
 }
 
 $userEnteredDiscoveryServiceUrl = Read-Host "Press Enter to accept the default DiscoveryService URL [$discoveryServiceUrl] or enter a new URL"
@@ -360,7 +369,7 @@ if(![string]::IsNullOrEmpty($userEnteredDiscoveryServiceUrl)){
 
 $userEnteredApplicationEndpoint = Read-Host "Press Enter to accept the default Application Endpoint URL [$applicationEndpoint] or enter a new URL"
 if(![string]::IsNullOrEmpty($userEnteredApplicationEndpoint)){
-	$applicationEndpoint = $userEnteredApplicationEndpoint
+    $applicationEndpoint = $userEnteredApplicationEndpoint
 }
 
 Unlock-ConfigurationSections
@@ -369,9 +378,9 @@ $appDirectory = "$webroot\$appName"
 New-AppRoot $appDirectory $iisUser
 Write-Console "App directory is: $appDirectory"
 if($useSpecificUser){
-	New-AppPool $appName $iisUser $credential
+    New-AppPool $appName $iisUser $credential
 }else{
-	New-AppPool $appName 
+    New-AppPool $appName 
 }
 
 Add-PermissionToPrivateKey $iisUser $signingCert read
@@ -386,21 +395,21 @@ $environmentVariables = @{"HostingOptions__StorageProvider" = "SqlServer"; "Host
 
 
 if($clientName){
-	$environmentVariables.Add("ClientName", $clientName)
+    $environmentVariables.Add("ClientName", $clientName)
 }
 
 if ($primarySigningCertificateThumbprint){
-	$environmentVariables.Add("SigningCertificateSettings__UseTemporarySigningCredential", "false")
-	$environmentVariables.Add("SigningCertificateSettings__PrimaryCertificateThumbprint", $primarySigningCertificateThumbprint)
+    $environmentVariables.Add("SigningCertificateSettings__UseTemporarySigningCredential", "false")
+    $environmentVariables.Add("SigningCertificateSettings__PrimaryCertificateThumbprint", $primarySigningCertificateThumbprint)
 }
 
 if ($encryptionCertificateThumbprint){
-	$environmentVariables.Add("SigningCertificateSettings__EncryptionCertificateThumbprint", $encryptionCertificateThumbprint)
+    $environmentVariables.Add("SigningCertificateSettings__EncryptionCertificateThumbprint", $encryptionCertificateThumbprint)
 }
 
 if($appInsightsInstrumentationKey){
-	$environmentVariables.Add("ApplicationInsights__Enabled", "true")
-	$environmentVariables.Add("ApplicationInsights__InstrumentationKey", $appInsightsInstrumentationKey)
+    $environmentVariables.Add("ApplicationInsights__Enabled", "true")
+    $environmentVariables.Add("ApplicationInsights__InstrumentationKey", $appInsightsInstrumentationKey)
 }
 
 $environmentVariables.Add("IdentityServerConfidentialClientSettings__Authority", "$applicationEndpoint")
@@ -459,6 +468,7 @@ if($metadataDbName){ Add-InstallationSetting "common" "metadataDbName" "$metadat
 if($identityDbName){ Add-InstallationSetting "identity" "identityDbName" "$identityDbName" }
 if($siteName){ Add-InstallationSetting "identity" "siteName" $siteName }
 if($primarySigningCertificateThumbprint){ Add-InstallationSetting "identity" "primarySigningCertificateThumbprint" $primarySigningCertificateThumbprint }
+if($iisUser){ Add-InstallationSetting "identity" "iisUser" "$iisUser" }
 
 Write-Console ""
 Write-Console "Please keep the following secrets in a secure place:"
