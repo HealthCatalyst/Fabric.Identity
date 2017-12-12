42f3ed878424b41a2a0c3755845c11468cb80d18
michael.vidal <michael.vidal@healthcatalyst.com>
2017-12-12 00:07:34 -0500

    Updated SqlServerPersistedGrantStore.StoreAsync method so that we check if the grant exists before adding it.

--- Fabric.Identity.API/Persistence/SqlServer/Stores/SqlServerPersistedGrantStore.cs
+++ Fabric.Identity.API/Persistence/SqlServer/Stores/SqlServerPersistedGrantStore.cs
@@ -5,6 +5,7 @@ using Fabric.Identity.API.Persistence.SqlServer.Services;
 using Microsoft.EntityFrameworkCore;
 using Fabric.Identity.API.Persistence.SqlServer.EntityModels;
 using Fabric.Identity.API.Persistence.SqlServer.Mappers;
+using Serilog;
 
 
 namespace Fabric.Identity.API.Persistence.SqlServer.Stores
@@ -12,10 +13,12 @@ namespace Fabric.Identity.API.Persistence.SqlServer.Stores
     public class SqlServerPersistedGrantStore : IPersistedGrantStore
     {
         private readonly IIdentityDbContext _identityDbContext;
+        private readonly ILogger _logger;
 
-        public SqlServerPersistedGrantStore(IIdentityDbContext identityDbContext)
+        public SqlServerPersistedGrantStore(IIdentityDbContext identityDbContext, ILogger logger)
         {
             _identityDbContext = identityDbContext;
+            _logger = logger;
         }
 
         public Task<IEnumerable<IdentityServer4.Models.PersistedGrant>> GetAllAsync(string subjectId)
@@ -64,14 +67,36 @@ namespace Fabric.Identity.API.Persistence.SqlServer.Stores
         private async Task DeletePersistedGrantsAsync(IQueryable<PersistedGrant> persistedGrants)
         {
             await persistedGrants.ForEachAsync(pg => _identityDbContext.PersistedGrants.Remove(pg));
-            await _identityDbContext.SaveChangesAsync();
+            try
+            {
+                await _identityDbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                _logger.Warning("Exception removing persistedGrants from the database. Error: {message}", ex.Message);
+            }
         }
 
         public async Task StoreAsync(IdentityServer4.Models.PersistedGrant grant)
         {
-            var persistedGrantEntity = grant.ToEntity();
-            _identityDbContext.PersistedGrants.Add(persistedGrantEntity);
-            await _identityDbContext.SaveChangesAsync();
+            var existingGrant = _identityDbContext.PersistedGrants.SingleOrDefault(pg => pg.Key == grant.Key);
+            if (existingGrant == null)
+            {
+                var persistedGrantEntity = grant.ToEntity();
+                _identityDbContext.PersistedGrants.Add(persistedGrantEntity);
+            }
+            else
+            {
+                grant.ToEntity(existingGrant);
+            }
+            try
+            {
+                await _identityDbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                _logger.Warning("Exception updating {grantKey}. Error: {error}", grant.Key, ex.Message);
+            }
         }
     }
 }
