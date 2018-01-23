51129bf2c33a0dc03310e5e86262ea2c6587e197
michael.vidal <michael.vidal@healthcatalyst.com>
2018-01-23 11:20:51 -0800

    Remove CouchDB integration tests since we are no longer supporting CouchDb.

--- Fabric.Identity.IntegrationTests/ControllerTests/CouchDb/CouchDbClientRegistrationTests.cs
+++ Fabric.Identity.IntegrationTests/ControllerTests/CouchDb/CouchDbClientRegistrationTests.cs
@@ -3,9 +3,9 @@ using Fabric.Identity.IntegrationTests.ControllerTests.InMemory;
 
 namespace Fabric.Identity.IntegrationTests.ControllerTests.CouchDb
 {
-    public class CouchDbClientRegistrationTests : ClientRegistrationTests
+    public class CouchDbClientRegistrationTests //: ClientRegistrationTests
     {
-        public CouchDbClientRegistrationTests() : base(FabricIdentityConstants.StorageProviders.CouchDb)
+        public CouchDbClientRegistrationTests() //: base(FabricIdentityConstants.StorageProviders.CouchDb)
         { }
     }
 }
