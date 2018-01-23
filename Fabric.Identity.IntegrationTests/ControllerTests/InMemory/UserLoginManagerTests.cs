201225132361e7f678f8bcf7c2c633bad84c8bca
michael.vidal <michael.vidal@healthcatalyst.com>
2018-01-23 00:41:40 -0500

    Better handle updates to claims, including scenario where claims are removed at the Idp. Add additional tests to cover scenarios.

--- Fabric.Identity.IntegrationTests/ControllerTests/InMemory/UserLoginManagerTests.cs
+++ Fabric.Identity.IntegrationTests/ControllerTests/InMemory/UserLoginManagerTests.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Security.Claims;
 using System.Text;
+using System.Threading;
 using Fabric.Identity.API;
 using Fabric.Identity.API.Management;
 using Fabric.Identity.API.Persistence;
@@ -61,14 +62,150 @@ namespace Fabric.Identity.IntegrationTests.ControllerTests.InMemory
             var user = await userLoginManager.UserLogin(provider, userId, claims, clientId);
 
             var firstLoginDate = user.LastLoginDatesByClient.First().LoginDate;
+            
+            //Sleep to ensure some time passes between logins
+            Thread.Sleep(10);
 
             user = await  userLoginManager.UserLogin(provider, userId, claims, clientId);
 
             var secondLoginDate = user.LastLoginDatesByClient.First().LoginDate;
+            
+            Assert.True(secondLoginDate >= firstLoginDate, "second login date is not greater than first");
+            
+        }
 
-            Assert.True(secondLoginDate > firstLoginDate, "second login date is not greater than first");
+        [Fact]
+        public async void TestUserLogin_AddClaims_Success()
+        {
+            var userLoginManager = new UserLoginManager(UserStore, new Mock<ILogger>().Object);
+            var userId = $"HealthCatalyst\\{GetRandomString()}";
+            var provider = FabricIdentityConstants.FabricExternalIdentityProviderTypes.Windows;
+            var clientId = "sampleApp";
+            var claims = GetBaseClaims();
 
+            await userLoginManager.UserLogin(provider, userId, claims, clientId);
+
+            var storedUser = UserStore.FindBySubjectIdAsync(userId).Result;
+
+            Assert.Equal(claims.Count, storedUser.Claims.Count);
+            Assert.Equal(claims.First(c => c.Type == ClaimTypes.Name).Value, storedUser.Claims.First(c => c.Type == JwtClaimTypes.Name).Value);
+            Assert.Equal(claims.First(c => c.Type == JwtClaimTypes.GivenName).Value, storedUser.Claims.First(c => c.Type == JwtClaimTypes.GivenName).Value);
+            Assert.Equal(claims.First(c => c.Type == JwtClaimTypes.FamilyName).Value, storedUser.Claims.First(c => c.Type == JwtClaimTypes.FamilyName).Value);
+            Assert.Equal(claims.First(c => c.Type == JwtClaimTypes.MiddleName).Value, storedUser.Claims.First(c => c.Type == JwtClaimTypes.MiddleName).Value);
+            Assert.Equal(claims.First(c => c.Type == JwtClaimTypes.Role).Value, storedUser.Claims.First(c => c.Type == JwtClaimTypes.Role).Value);
 
         }
+
+        [Fact]
+        public async void TestUserLogin_UpdateClaims_Success()
+        {
+            var userLoginManager = new UserLoginManager(UserStore, new Mock<ILogger>().Object);
+            var userId = $"HealthCatalyst\\{GetRandomString()}";
+            var provider = FabricIdentityConstants.FabricExternalIdentityProviderTypes.Windows;
+            var clientId = "sampleApp";
+            var claims = GetBaseClaims();
+
+            await userLoginManager.UserLogin(provider, userId, claims, clientId);
+
+            var storedUser = UserStore.FindBySubjectIdAsync(userId).Result;
+
+            Assert.Equal(claims.Count, storedUser.Claims.Count);
+
+            var claim = claims.FirstOrDefault(c => c.Type == JwtClaimTypes.FamilyName);
+            claims.Remove(claim);
+            claim = new Claim(JwtClaimTypes.FamilyName, "bar");
+            claims.Add(claim);
+            await userLoginManager.UserLogin(provider, userId, claims, clientId);
+
+            storedUser = UserStore.FindBySubjectIdAsync(userId).Result;
+            Assert.Equal(claims.Count, storedUser.Claims.Count);
+            Assert.Equal(claims.First(c => c.Type == ClaimTypes.Name).Value, storedUser.Claims.First(c => c.Type == JwtClaimTypes.Name).Value);
+            Assert.Equal(claims.First(c => c.Type == JwtClaimTypes.GivenName).Value, storedUser.Claims.First(c => c.Type == JwtClaimTypes.GivenName).Value);
+            Assert.Equal(claim.Value, storedUser.Claims.First(c => c.Type == JwtClaimTypes.FamilyName).Value);
+            Assert.Equal(claims.First(c => c.Type == JwtClaimTypes.MiddleName).Value, storedUser.Claims.First(c => c.Type == JwtClaimTypes.MiddleName).Value);
+            Assert.Equal(claims.First(c => c.Type == JwtClaimTypes.Role).Value, storedUser.Claims.First(c => c.Type == JwtClaimTypes.Role).Value);
+
+        }
+
+        [Fact]
+        public async void TestUserLogin_UpdateClaims_AddsNewClaim()
+        {
+            var userLoginManager = new UserLoginManager(UserStore, new Mock<ILogger>().Object);
+            var userId = $"HealthCatalyst\\{GetRandomString()}";
+            var provider = FabricIdentityConstants.FabricExternalIdentityProviderTypes.Windows;
+            var clientId = "sampleApp";
+            var claims = GetBaseClaims();
+
+            await userLoginManager.UserLogin(provider, userId, claims, clientId);
+
+            var storedUser = UserStore.FindBySubjectIdAsync(userId).Result;
+
+            Assert.Equal(claims.Count, storedUser.Claims.Count);
+
+            var claim = new Claim(JwtClaimTypes.Role, @"FABRIC\Health Catalyst Editor");
+            claims.Add(claim);
+            await userLoginManager.UserLogin(provider, userId, claims, clientId);
+
+            storedUser = UserStore.FindBySubjectIdAsync(userId).Result;
+            Assert.Equal(claims.Count, storedUser.Claims.Count);
+            Assert.Equal(claims.First(c => c.Type == ClaimTypes.Name).Value, storedUser.Claims.First(c => c.Type == JwtClaimTypes.Name).Value);
+            Assert.Equal(claims.First(c => c.Type == JwtClaimTypes.GivenName).Value, storedUser.Claims.First(c => c.Type == JwtClaimTypes.GivenName).Value);
+            Assert.Equal(claims.First(c => c.Type == JwtClaimTypes.FamilyName).Value, storedUser.Claims.First(c => c.Type == JwtClaimTypes.FamilyName).Value);
+            Assert.Equal(claims.First(c => c.Type == JwtClaimTypes.MiddleName).Value, storedUser.Claims.First(c => c.Type == JwtClaimTypes.MiddleName).Value);
+            Assert.Equal(claims.Count(c => c.Type == JwtClaimTypes.Role), storedUser.Claims.Count(c => c.Type == JwtClaimTypes.Role));
+            Assert.Equal(2, storedUser.Claims.Count(c => c.Type == JwtClaimTypes.Role));
+
+        }
+
+        [Fact]
+        public async void TestUserLogin_UpdateClaims_RemovesClaim()
+        {
+            var userLoginManager = new UserLoginManager(UserStore, new Mock<ILogger>().Object);
+            var userId = $"HealthCatalyst\\{GetRandomString()}";
+            var provider = FabricIdentityConstants.FabricExternalIdentityProviderTypes.Windows;
+            var clientId = "sampleApp";
+            var claims = GetBaseClaims();
+            claims.Add(new Claim(JwtClaimTypes.Role, @"FABRIC\Health Catalyst Editor"));
+
+            await userLoginManager.UserLogin(provider, userId, claims, clientId);
+
+            var storedUser = UserStore.FindBySubjectIdAsync(userId).Result;
+
+            Assert.Equal(claims.Count, storedUser.Claims.Count);
+
+            var claim = claims.FirstOrDefault(c => c.Type == JwtClaimTypes.Role);
+            claims.Remove(claim);
+            await userLoginManager.UserLogin(provider, userId, claims, clientId);
+
+            storedUser = UserStore.FindBySubjectIdAsync(userId).Result;
+            Assert.Equal(claims.Count, storedUser.Claims.Count);
+            Assert.Equal(claims.First(c => c.Type == ClaimTypes.Name).Value, storedUser.Claims.First(c => c.Type == JwtClaimTypes.Name).Value);
+            Assert.Equal(claims.First(c => c.Type == JwtClaimTypes.GivenName).Value, storedUser.Claims.First(c => c.Type == JwtClaimTypes.GivenName).Value);
+            Assert.Equal(claims.First(c => c.Type == JwtClaimTypes.FamilyName).Value, storedUser.Claims.First(c => c.Type == JwtClaimTypes.FamilyName).Value);
+            Assert.Equal(claims.First(c => c.Type == JwtClaimTypes.MiddleName).Value, storedUser.Claims.First(c => c.Type == JwtClaimTypes.MiddleName).Value);
+            Assert.Equal(claims.Count(c => c.Type == JwtClaimTypes.Role), storedUser.Claims.Count(c => c.Type == JwtClaimTypes.Role));
+            Assert.Equal(1, storedUser.Claims.Count(c => c.Type == JwtClaimTypes.Role));
+
+        }
+
+        private List<Claim> GetBaseClaims()
+        {
+            var userNameClaim = new Claim(ClaimTypes.Name, "foo baz");
+            var firstNameClaim = new Claim(JwtClaimTypes.GivenName, "foo");
+            var lastNameClaim = new Claim(JwtClaimTypes.FamilyName, "baz");
+            var middleNameClaim = new Claim(JwtClaimTypes.MiddleName, "dot");
+            var roleClaim = new Claim(JwtClaimTypes.Role, @"FABRIC\Health Catalyst Viewer");
+            var claims = new List<Claim>
+            {
+                userNameClaim,
+                firstNameClaim,
+                lastNameClaim,
+                middleNameClaim,
+                roleClaim
+            };
+            return claims;
+        }
+
+
     }
 }
