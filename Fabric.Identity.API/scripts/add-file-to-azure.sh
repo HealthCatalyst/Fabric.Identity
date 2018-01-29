20db517f56357ef8f96a54db2ad2b0687a68958d
Kyle Paul <djjoebanks@gmail.com>
2018-01-29 15:00:28 -0800

    add script to push a file into azure storage container

---
+++ Fabric.Identity.API/scripts/add-file-to-azure.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+storageAccountName=$1
+storageAccountKey=$2
+blobName=$3
+fileToUpload=$4
+
+
+export AZURE_STORAGE_ACCOUNT=storageAccountName
+export AZURE_STORAGE_ACCESS_KEY=storageAccountKey
+
+export container_name=fabric-release
+export blob_name=blobName
+export file_to_upload=fileToUpload
+
+echo "Creating the container..."
+az storage container create --name $container_name
+
+echo "Uploading the file..."
+az storage blob upload --container-name $container_name --file $file_to_upload --name $blob_name
+
+echo "Listing the blobs..."
+az storage blob list --container-name $container_name --output table
+
+echo "Done"
