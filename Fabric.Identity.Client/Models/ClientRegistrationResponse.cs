3b074b8c3e0bc03b02def2f03599fa589fc55928
michael.vidal <michael.vidal@healthcatalyst.com>
2018-03-09 11:50:09 -0500

    Added a function to register a client with Fabric.Identity.

---
+++ Fabric.Identity.Client/Models/ClientRegistrationResponse.cs
@@ -0,0 +1,23 @@
+﻿namespace Fabric.Identity.Client.Models
+{
+    /// <summary>
+    /// The client registration response.
+    /// </summary>
+    public class ClientRegistrationResponse
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether there is an error.
+        /// </summary>
+        public bool IsError { get; set; }
+
+        /// <summary>
+        /// Gets or sets the client.
+        /// </summary>
+        public Client Client { get; set; }
+
+        /// <summary>
+        /// Gets or sets the error.
+        /// </summary>
+        public Error Error { get; set; }
+    }
+}
