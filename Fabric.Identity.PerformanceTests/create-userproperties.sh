780ebd678b90828e48c756f6b91fe4afee6ae5dc
michael.vidal <michael.vidal@healthcatalyst.com>
2018-03-20 09:45:07 -0400

    Updated perf tests to take in a protocol parameter so we can perf test over https.

--- Fabric.Identity.PerformanceTests/create-userproperties.sh
+++ Fabric.Identity.PerformanceTests/create-userproperties.sh
@@ -5,9 +5,16 @@ authport=$2
 identityserver=$3
 identityport=$4
 
+identityprotocol="http"
+
+if [ $5 ]; then
+    identityprotocol=$5
+fi
+
 cat > user.properties << EOF
 identityserver=$identityserver
 identityport=$identityport
+identityprotocol=$identityprotocol
 authserver=$authserver
 authport=$authport
 EOF
