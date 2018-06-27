faa53a2c93d365d6bd0b4256ad97066f224d7e3e
Jason Soto <jason.soto@healthcatalyst.com>
2018-06-27 16:53:51 -0600

    Update filepath for scripts in functional tests

--- Fabric.Identity.API/scripts/tests/identity-cli.functional.tests.ps1
+++ Fabric.Identity.API/scripts/tests/identity-cli.functional.tests.ps1
@@ -1,15 +1,19 @@
 param(
+    [string] $targetFilePath = "$PSScriptRoot\..\identity-cli.psm1",
     [Uri] $identityUrl = "http://localhost:5001"
 )
+
+# Force re-import to pick up latest changes
+Import-Module $targetFilePath -Force
 
 describe 'Functional-Test' {    
     BeforeAll {
         # Set up auth/identity
-        Start-Process ..\start-auth-identity.sh -Wait
+        Start-Process "$targetFilePath\..\start-auth-identity.sh" -Wait
     }
     AfterAll {
         # Tear down auth/identity
-        Start-Process ..\stop-auth-identity.sh -Wait
+        Start-Process "$targetFilePath\..\stop-auth-identity.sh" -Wait
     }
     
     It 'Should return an access token when valid response' {
