d7455ee7d99029a36fd94890fd9547669e3a731d
michael.vidal <michael.vidal@healthcatalyst.com>
2018-08-27 13:20:08 -0400

    Code completed for v2 of identity installation script.

--- Fabric.Identity.API/scripts/Install-Identity.ps1
+++ Fabric.Identity.API/scripts/Install-Identity.ps1
@@ -32,12 +32,48 @@ if(!$noDiscoveryService){
 $identityServiceUrl = Get-ApplicationEndpoint -appName $installSettings.appName -applicationEndpoint $installSettings.applicationEndPoint -quiet $quiet
 
 Unlock-ConfigurationSections
-$version = Publish-Identity -site $selectedSite `
+$installApplication = Publish-Identity -site $selectedSite `
                  -appName $installSettings.appName `
                  -iisUser $iisUser `
                  -zipPackage $zipPackage `
 
 Add-DatabaseSecurity $iisUser.UserName $installSettings.identityDatabaseRole $identityDatabase.DbConnectionString
 if(!$noDiscoveryService){
-    Register-IdentityWithDiscovery -iisUserName $iisUser.UserName -metadataConnStr $metadataDatabase.DbConnectionString -version $version -identityServerUrl $identityServiceUrl
+    Register-IdentityWithDiscovery -iisUserName $iisUser.UserName -metadataConnStr $metadataDatabase.DbConnectionString -version $installApplication.version -identityServerUrl $identityServiceUrl
 }
+
+Set-IdentityEnvironmentVariables -appDirectory $installApplication.applicationDirectory `
+-primarySigningCertificateThumbprint $selectedCerts.SigningCertificate.Thumbprint `
+-encryptionCertificateThumbprint $selectedCerts.EncryptionCertificate.Thumbprint `
+-appInsightsInstrumentationKey $appInsightsKey `
+-applicationEndpoint $identityServiceUrl `
+-identityDbConnStr $identityDatabase.DbConnectionString`
+-discoveryServiceUrl $discoveryServiceUrl `
+-noDiscoveryService $noDiscoveryService
+
+$accessToken = ""
+
+if(Test-RegistrationComplete $identityServerUrl) {
+    $accessToken = Get-AccessToken -authUrl $identityServerUrl -clientId "fabric-installer" -scope "fabric/identity.manageresources" -secret $installSettings.fabricInstallerSecret
+}
+
+$registrationApiSecret = Add-RegistrationApiRegistration -identityServerUrl $identityServiceUrl -accessToken $accessToken
+$fabricInstallerSecret = Add-InstallerClientRegistration -identityServerUrl $identityServiceUrl -accessToken $accessToken -fabricInstallerSecret $installSettings.fabricInstallerSecret
+Add-SecureInstallationSetting "common" "fabricInstallerSecret" $fabricInstallerSecret $selectedCerts.SigningCertificate
+
+if (!$accessToken){
+    $accessToken = Get-AccessToken -authUrl $identityServiceUrl -clientId "fabric-installer" -scope "fabric/identity.manageresources" -secret $fabricInstallerSecret
+}
+
+$identityClientSecret = Add-IdentityClientRegistration -identityServerUrl $identityServiceUrl -accessToken $accessToken
+Add-SecureIdentityEnvironmentVariables -encryptionCert $selectedCerts.SigningCertificate `
+    -identityClientSecret $identityClientSecret `
+    -registrationApiSecret $registrationApiSecret `
+    -appDirectory $installApplication.applicationDirectory
+
+if ($fabricInstallerSecret){
+    Write-DosMessage -Level "Information" -Message "Please keep the following Fabric.Installer secret in a secure place, it will be needed in subsequent installations:"
+    Write-DosMessage -Level "Information" -Message "Fabric.Installer clientSecret: $fabricInstallerSecret"
+}
+
+Read-Host -Prompt "Installation complete, press Enter to exit"
