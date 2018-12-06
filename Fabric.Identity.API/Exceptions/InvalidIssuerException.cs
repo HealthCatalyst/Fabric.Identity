dbf260e2034132c9c149f1973c29b98721ea9899
Jorden Lowe <jorden.lowe@healthcatalyst.com>
2018-12-06 09:40:11 -0500

    Refactored code to allow unit tests.

---
+++ Fabric.Identity.API/Exceptions/InvalidIssuerException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Fabric.Identity.API.Exceptions
+{
+    public class InvalidIssuerException : Exception
+    {
+        public InvalidIssuerException()
+        {
+        }
+        public InvalidIssuerException(string message) : base(message)
+        {
+        }
+
+        public InvalidIssuerException(string message, Exception innerException) : base(message,
+            innerException)
+        {
+        }
+
+        public string LogMessage { get; set; }
+    }
+}
---
+++ Fabric.Identity.API/Models/ClaimsResult.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using Microsoft.AspNetCore.Http.Authentication;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace Fabric.Identity.API.Models
+{
+    public class ClaimsResult
+    {
+        public string Provider { get; internal set; }
+        public string UserId { get; internal set; }
+        public string ClientId { get; internal set; }
+        public AuthenticationProperties AuthenticationProperties { get; internal set; }
+        public Claim[] AdditionalClaims { get; internal set; }
+        public string SchemeItem { get; internal set; }
+        public List<Claim> Claims { get; internal set; }
+        public Claim UserIdClaim { get; internal set; }
+    }
+}
