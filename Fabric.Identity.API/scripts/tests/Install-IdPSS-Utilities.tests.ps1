727a9a8ae1e3c15b2519e67b5998b61ff0e20dba
Jason Soto <jason.soto@healthcatalyst.com>
2018-12-06 14:14:07 -0700

    Make Install-Identity not require install-idpss-utlities
    Update Install-idpss-utilities to not throw errors when AzureAD module is not installed

--- Fabric.Identity.API/scripts/tests/Install-IdPSS-Utilities.tests.ps1
+++ Fabric.Identity.API/scripts/tests/Install-IdPSS-Utilities.tests.ps1
@@ -112,28 +112,6 @@ Describe 'New-FabricAzureADApplication' -Tag 'Unit' {
     }
 }
 
-Describe 'Get-ClientSettingsFromInstallConfig' -Tag 'Unit' {
-    Context 'Valid config path' {
-        It 'should return a list of client settings' {
-            $mockXml = [xml]'<?xml version="1.0" encoding="utf-8"?><installation><settings><scope name="identity"><variable name="fabricInstallerSecret" value="" /><variable name="discoveryService" value="" />	<registeredApplications><variable appName="testApp" tenantId="tenant1" secret="secret1" clientid="clientid1" /><variable appName="testApp" tenantId="tenant2" secret="secret2" clientid="clientid2" /></registeredApplications></scope></settings></installation>'
-
-            Mock -CommandName Get-Content { return $mockXml }
-            $result = Get-ClientSettingsFromInstallConfig -installConfigPath $targetFilePath -appName "testApp"
-            $result.length | Should -Be 2
-            $firstApp = $result[0]
-            $secondApp = $result[1]
-
-            $firstApp.clientId | Should -Be "clientid1"
-            $firstApp.tenantId | Should -Be "tenant1"
-            $firstApp.clientSecret | Should -Be "secret1"
-
-            $secondApp.clientId | Should -Be "clientid2"
-            $secondApp.tenantId | Should -Be "tenant2"
-            $secondApp.clientSecret | Should -Be "secret2"
-        }
-    }
-}
-
 Describe 'Get-SettingsFromInstallConfig' -Tag 'Unit' {
     Context 'Section Exists' {
         It 'should return a list of settings' {
@@ -182,7 +160,6 @@ Describe 'Get-ReplyUrls' -Tag 'Unit' {
     Context 'Urls do not exist in config' {
         InModuleScope Install-IdPSS-Utilities {
             It 'Should throw when no replyUrl in install.config' {
-                Mock Add-NestedSetting {}
                 Mock -ModuleName Install-IdPSS-Utilities -CommandName Get-SettingsFromInstallConfig {}
                 { Get-ReplyUrls -installConfigPath $targetFilePath } | Should -Throw
             }
