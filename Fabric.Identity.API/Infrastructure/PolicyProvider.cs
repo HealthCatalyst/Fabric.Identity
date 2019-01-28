f532a427cc4b8383c243900d3db1ce3880df432b
Brian Smith <brian.smith@healthcatalyst.com>
2019-01-28 14:46:23 -0500

    added RetryPolicy and wrapped CircuitBreakerPolicy with it to use in IdP SS search requests

--- Fabric.Identity.API/Infrastructure/PolicyProvider.cs
+++ Fabric.Identity.API/Infrastructure/PolicyProvider.cs
@@ -3,6 +3,8 @@ using System.Net.Http;
 using Novell.Directory.Ldap;
 using Polly;
 using Polly.CircuitBreaker;
+using Polly.Retry;
+using Polly.Wrap;
 
 namespace Fabric.Identity.API.Infrastructure
 {
@@ -11,7 +13,17 @@ namespace Fabric.Identity.API.Infrastructure
         public readonly CircuitBreakerPolicy LdapErrorPolicy = Policy.Handle<LdapException>()
             .CircuitBreaker(5, TimeSpan.FromMinutes(5));
 
+        public readonly RetryPolicy IdPSearchServiceRetryPolicy = Policy.Handle<HttpRequestException>()
+            .RetryAsync(3);
+
         public readonly CircuitBreakerPolicy IdPSearchServiceErrorPolicy = Policy.Handle<HttpRequestException>()
-            .CircuitBreakerAsync(5, TimeSpan.FromMinutes(5));
+            .CircuitBreakerAsync(3, TimeSpan.FromMinutes(1));
+
+        public readonly PolicyWrap IdPSearchServicePolicy;
+
+        public PolicyProvider()
+        {
+            IdPSearchServicePolicy = IdPSearchServiceRetryPolicy.WrapAsync(IdPSearchServiceErrorPolicy);
+        }
     }
 }
