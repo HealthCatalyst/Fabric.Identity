dcf89e7c4bc371fd479655a311c214ba15b016c8
Brian Smith <brian.smith@healthcatalyst.com>
2019-01-28 17:39:57 -0500

    added explicit timeout to IdP SS HTTP request

--- Fabric.Identity.API/Infrastructure/PolicyProvider.cs
+++ Fabric.Identity.API/Infrastructure/PolicyProvider.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Http;
+using System.Threading.Tasks;
 using Novell.Directory.Ldap;
 using Polly;
 using Polly.CircuitBreaker;
@@ -13,16 +14,19 @@ namespace Fabric.Identity.API.Infrastructure
         public readonly CircuitBreakerPolicy LdapErrorPolicy = Policy.Handle<LdapException>()
             .CircuitBreaker(5, TimeSpan.FromMinutes(5));
 
-        public readonly RetryPolicy IdPSearchServiceRetryPolicy = Policy.Handle<HttpRequestException>()
-            .RetryAsync(3);
+        private readonly PolicyBuilder _idPSearchServicePolicyBuilder = Policy.Handle<HttpRequestException>()
+            .Or<TaskCanceledException>();
 
-        public readonly CircuitBreakerPolicy IdPSearchServiceErrorPolicy = Policy.Handle<HttpRequestException>()
-            .CircuitBreakerAsync(3, TimeSpan.FromMinutes(1));
+        public readonly RetryPolicy IdPSearchServiceRetryPolicy;
+        public readonly CircuitBreakerPolicy IdPSearchServiceErrorPolicy;
+            
 
         public readonly PolicyWrap IdPSearchServicePolicy;
 
         public PolicyProvider()
         {
+            IdPSearchServiceRetryPolicy = _idPSearchServicePolicyBuilder.RetryAsync(3);
+            IdPSearchServiceErrorPolicy = _idPSearchServicePolicyBuilder.CircuitBreakerAsync(3, TimeSpan.FromMinutes(5));
             IdPSearchServicePolicy = IdPSearchServiceRetryPolicy.WrapAsync(IdPSearchServiceErrorPolicy);
         }
     }
