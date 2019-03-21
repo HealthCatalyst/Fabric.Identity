adf8454071240a174d51df831fecc8c8dc50c91e
Ross Lipenta <ross.lipenta@healthcatalyst.com>
2019-03-21 09:55:44 -0400

    Update install scripts to use Get-WebRequestDownload
    Replace Invoke-WebRequest calls to DosInstallUtilities new Get-WebRequestDownload function which wraps Invoke-WebRequest and ensures no progress bar is shown when downloading

--- Fabric.Identity.API/scripts/Install-Identity.ps1
+++ Fabric.Identity.API/scripts/Install-Identity.ps1
@@ -23,7 +23,7 @@ Import-Module -Name .\Install-Identity-Utilities.psm1 -Force
 $fabricInstallUtilities = ".\Fabric-Install-Utilities.psm1"
 if (!(Test-Path $fabricInstallUtilities -PathType Leaf)) {
     Write-DosMessage -Level "Warning" -Message "Could not find fabric install utilities. Manually downloading and installing"
-    Invoke-WebRequest -Uri https://raw.githubusercontent.com/HealthCatalyst/InstallScripts/master/common/Fabric-Install-Utilities.psm1 -Headers @{"Cache-Control" = "no-cache"} -OutFile $fabricInstallUtilities
+    Get-WebRequestDownload -Uri https://raw.githubusercontent.com/HealthCatalyst/InstallScripts/master/common/Fabric-Install-Utilities.psm1 -NoCache -OutFile $fabricInstallUtilities
 }
 Import-Module -Name $fabricInstallUtilities -Force
 
--- Fabric.Identity.API/scripts/tests/Install-Identity-Utilities.tests.ps1
+++ Fabric.Identity.API/scripts/tests/Install-Identity-Utilities.tests.ps1
@@ -34,20 +34,20 @@ Describe 'Install-DotNetCoreIfNeeded' -Tag 'Unit' {
     Context 'DotNetCore version is already installed'{
         It 'Should Not Install'{
             Mock -ModuleName Install-Identity-Utilities -CommandName Test-PrerequisiteExact -MockWith { return $true }
-            Mock -ModuleName Install-Identity-Utilities -CommandName Invoke-WebRequest -MockWith {}
+            Mock -ModuleName Install-Identity-Utilities -CommandName Get-WebRequestDownload -MockWith {}
             Install-DotNetCoreIfNeeded -version "1.1.1234.123" -downloadUrl "http://example.com"
-            Assert-MockCalled -ModuleName Install-Identity-Utilities -CommandName Invoke-WebRequest -Times 0 -Exactly
+            Assert-MockCalled -ModuleName Install-Identity-Utilities -CommandName Get-WebRequestDownload -Times 0 -Exactly
 
         }
         InModuleScope Install-Identity-Utilities {
             It 'Should install proper version' {
                 Mock -CommandName Test-PrerequisiteExact -MockWith { return $false }
-                Mock -CommandName Invoke-WebRequest -MockWith {}
+                Mock -CommandName Get-WebRequestDownload -MockWith {}
                 Mock -CommandName Start-Process -MockWith {}
                 Mock -CommandName Restart-W3SVC -MockWith {}
                 Mock -CommandName Remove-Item -MockWith {}
                 Install-DotNetCoreIfNeeded -version "1.1.1234.123" -downloadUrl "http://example.com"
-                Assert-MockCalled -CommandName Invoke-WebRequest -Times 1 -Exactly
+                Assert-MockCalled -CommandName Get-WebRequestDownload -Times 1 -Exactly
                 Assert-MockCalled -CommandName Start-Process -Times 1 -Exactly
                 Assert-MockCalled -CommandName Restart-W3SVC -Times 1 -Exactly
                 Assert-MockCalled -CommandName Remove-Item -Times 1 -Exactly
@@ -55,7 +55,7 @@ Describe 'Install-DotNetCoreIfNeeded' -Tag 'Unit' {
 
             It 'Should throw an exception if Start-Process throws' {
                 Mock -CommandName Test-PrerequisiteExact -MockWith { return $false }
-                Mock -CommandName Invoke-WebRequest -MockWith {}
+                Mock -CommandName Get-WebRequestDownload -MockWith {}
                 Mock -CommandName Start-Process -MockWith { throw }
                 Mock -CommandName Restart-W3SVC -MockWith {}
                 Mock -CommandName Remove-Item -MockWith {}
