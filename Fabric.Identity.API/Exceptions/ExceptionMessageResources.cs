3c92cbbfb6ff0016e9e3295e7a13465b7e63913d
Brian Smith <brian.smith@healthcatalyst.com>
2019-03-26 15:07:40 -0400

    fixed typo in exception message

--- Fabric.Identity.API/Exceptions/ExceptionMessageResources.cs
+++ Fabric.Identity.API/Exceptions/ExceptionMessageResources.cs
@@ -3,7 +3,7 @@
     public static class ExceptionMessageResources
     {
         public static string MissingUserClaimMessage = "No User claim present on token";
-        public static string MissingIssuerClaimMessage = "No Issuerer claim present on token";
+        public static string MissingIssuerClaimMessage = "No Issuer claim present on token";
         public static string ForbiddenIssuerMessageLog = "The Issuer on the claim ({0}) is not in the Issuer White List";
         public static string ForbiddenIssuerMessageUser = "Cannot access because the site is not configured for your account. Please contact your administrator for assistance";
     }
