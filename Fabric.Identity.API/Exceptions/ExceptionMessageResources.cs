0b06f0b2980350ed3a128b7bc900afa650d4517d
Brian Smith <brian.smith@healthcatalyst.com>
2019-03-29 12:37:48 -0400

    updated error message

--- Fabric.Identity.API/Exceptions/ExceptionMessageResources.cs
+++ Fabric.Identity.API/Exceptions/ExceptionMessageResources.cs
@@ -5,6 +5,6 @@
         public static string MissingUserClaimMessage = "No User claim present on token";
         public static string MissingIssuerClaimMessage = "No Issuer claim present on token";
         public static string ForbiddenIssuerMessageLog = "The Issuer on the claim ({0}) is not in the Issuer White List";
-        public static string ForbiddenIssuerMessageUser = "Cannot access because the site is not configured for your account. Please contact your administrator for assistance";
+        public static string ForbiddenIssuerMessageUser = "Access denied. Try closing the browser and logging in with a different user, or contact your administrator for assistance.";
     }
 }
