177ccbaa4cbd29d05f95cde2527cbfa7b882647b
Josh Nielsen <joshua.nielsen@healthcatalayst.com>
2019-04-16 09:55:48 -0600

    Changed from getting user domain to machine domain
    Changed the script from getting the logged in users domain to now getting the machine domain that is being used.

--- Fabric.Identity.API/scripts/Install-Identity-Utilities.psm1
+++ Fabric.Identity.API/scripts/Install-Identity-Utilities.psm1
@@ -1395,7 +1395,10 @@ function Get-CurrentUserDomain
         [Parameter(Mandatory=$true)]
         [bool] $quiet
     )
-    $currentUserDomain = $env:userdomain
+    $currentUserDomain = (Get-WmiObject Win32_ComputerSystem).Domain.toUpper()
+    if($currentUserDomain.contains('.')) {
+        $currentUserDomain = $currentUserDomain.Split('.')[0]
+    }
     if(!$quiet){
         $userEnteredDomain = Read-Host "Press Enter to accept the default domain '$($currentUserDomain)' that the user/group who will administrate dos is a member or enter a new domain" 
         if (![string]::IsNullOrEmpty($userEnteredDomain)) {
