c4a6fbc95125d4f78ac9af1e0bdd576d5e9555ce
Jason Soto <jason.soto@healthcatalyst.com>
2019-09-04 10:44:43 -0600

    Add certificate validation function and tests

--- Fabric.Identity.API/scripts/tests/Install-Identity-Utilities.tests.ps1
+++ Fabric.Identity.API/scripts/tests/Install-Identity-Utilities.tests.ps1
@@ -1108,3 +1108,43 @@ Describe 'New-IdentityEncryptionCertificate ' {
         }
     }
 }
+
+Describe 'Test-IdentityEncryptionCertificate' {
+    It 'Should return false when cert is expired' {
+        # Arrange
+        $testDate = Get-Date
+        $testCert = New-MockObject System.Security.Cryptography.X509Certificates.x509Certificate2
+        $expirationProperty = @{
+            MemberType = [System.Management.Automation.PSMemberTypes]::NoteProperty
+            Name = "NotAfter"
+            value = $testDate.AddDays(-1)
+            Force = $true
+        }
+        $testCert | Add-Member @expirationProperty -ErrorAction 0
+
+        # Act
+        $value = Test-IdentityEncryptionCertificateValid -encryptionCertificate $testCert
+
+        # Assert
+        $value | Should -Be $false
+    }
+
+    It 'Should return true when cert is not expired' {
+        # Arrange
+        $testDate = Get-Date
+        $testCert = New-MockObject System.Security.Cryptography.X509Certificates.x509Certificate2
+        $expirationProperty = @{
+            MemberType = [System.Management.Automation.PSMemberTypes]::NoteProperty
+            Name = "NotAfter"
+            value = $testDate.AddDays(10)
+            Force = $true
+        }
+        $testCert | Add-Member @expirationProperty -ErrorAction 0
+
+        # Act
+        $value = Test-IdentityEncryptionCertificateValid -encryptionCertificate $testCert
+
+        # Assert
+        $value | Should -Be $true
+    }
+}
