192debdd520a5b9cc3572cc77984c7ffe87e7088
Kendall Miller <kendall.miller@healthcatalyst.com>
2019-09-05 18:56:24 -0600

    another change to trigger build

--- Fabric.Identity.API/scripts/tests/Install-Identity-Utilities.tests.ps1
+++ Fabric.Identity.API/scripts/tests/Install-Identity-Utilities.tests.ps1
@@ -4,6 +4,7 @@ param(
 
 Write-Host $targetFilePath
 # Force re-import to pick up latest changes
+# one more change
 Import-Module $targetFilePath -Force
 
 Describe "Identity Utilities Unit and Integration Tests" {
