64f61bb414355fe4e598879c2c949833ae11625e
Kendall Miller <kendall.miller@healthcatalyst.com>
2019-09-06 18:47:21 -0600

    trigger build

--- Fabric.Identity.API/scripts/tests/Install-Identity-Utilities.tests.ps1
+++ Fabric.Identity.API/scripts/tests/Install-Identity-Utilities.tests.ps1
@@ -808,6 +808,56 @@ Describe 'Get-ClientSettingsFromInstallConfig' -Tag 'Unit' {
   }
 }
 
+Describe 'Get-WebDeployPackagePath'{
+    Context 'Standalone'{
+        InModuleScope Install-Identity-Utilities{
+            It 'Should return the standalone path'{
+                # Arrange
+                $standAlonePath = ".\Catalyst.DiscoveryService.zip"
+                $resolvedPath = "C:\Installer\Catalyst.DiscoveryService.zip"
+                Mock Resolve-Path { return $resolvedPath }
+                Mock Test-Path -ParameterFilter { $Path -eq $standAlonePath } { return $true }
+
+                # Act
+                $path = Get-WebDeployPackagePath -standalonePath $standAlonePath -installerPath $resolvedPath
+
+                # Assert
+                $path | Should -Be $resolvedPath
+            }
+        }
+    }
+    Context 'Standalone'{
+        InModuleScope Install-Identity-Utilities{
+            It 'Should return the installer path'{
+                # Arrange
+                $installerPath = "..\WebDeployPackages\Catalyst.DiscoveryService.zip"
+                $resolvedPath = "C:\Installer\WebDeployPackages\Catalyst.DiscoveryService.zip"
+                Mock Resolve-Path { return $resolvedPath }
+                Mock Test-Path -ParameterFilter { $Path -eq $installerPath } { return $true }
+
+                # Act
+                $path = Get-WebDeployPackagePath -standalonePath $resolvedPath -installerPath $installerPath
+
+                # Assert
+                $path | Should -Be $resolvedPath
+            }
+        }
+    }
+    Context 'Failure'{
+        InModuleScope Install-Identity-Utilities{
+            It 'Should throw an exception'{
+                # Arrange
+                $installerPath = "..\WebDeployPackages\Catalyst.DiscoveryService.zip"
+                $resolvedPath = "C:\Installer\WebDeployPackages\Catalyst.DiscoveryService.zip"
+                Mock Test-Path { return $false }
+
+                # Act/Assert
+                { Get-WebDeployPackagePath -standalonePath $resolvedPath -installerPath $installerPath } | Should -Throw
+            }
+        }
+    }
+}
+
 Describe 'Test-DiscoveryService Unit Tests'{
     InModuleScope Install-Identity-Utilities{
     Context 'Success'{
