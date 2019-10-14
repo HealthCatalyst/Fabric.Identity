58fcdafb07339d045c93374d42ece1ef035b25de
Jason Soto <jason.soto@healthcatalyst.com>
2019-10-14 11:52:36 -0600

    Merge into idpss tests into one file
    There is an issue with our build pipeline that causes these to fail if split out too much

--- Fabric.Identity.API/scripts/tests/Install-IdPSS-Utilities.tests.ps1
+++ Fabric.Identity.API/scripts/tests/Install-IdPSS-Utilities.tests.ps1
@@ -173,6 +173,123 @@ Describe 'IdPSS Unit Tests' {
             }
         }
     }
+
+    Describe 'New-FabricAzureADApplication' -Tag 'Unit' {
+        InModuleScope Install-IdPSS-Utilities {
+            Context 'Happy Paths' {
+                BeforeAll {
+                    $existingResourceAccess = [Microsoft.Open.AzureAD.Model.ResourceAccess]@{
+                        Id = "existing-id"
+                        Type = "Role"
+                    }
+                    $newResourceAccess = [Microsoft.Open.AzureAD.Model.ResourceAccess]@{
+                        Id = "new-id"
+                        Type = "Role"
+                    }
+                    $emptyRequiredResourceAccessList = @()
+                }
+                It 'should create a new Azure application' {
+                    Mock -CommandName Get-AzureADApplication {}
+                    Mock -CommandName Connect-AzureAD {}
+                    Mock -CommandName New-AzureADApplication {}
+                    Mock -CommandName Set-AzureADApplication {}
+                    Mock -CommandName Get-AzureADServicePrincipal {return $returnPrincipal}
+    
+                    New-FabricAzureADApplication -appName "app" -replyUrls @(@{name="url"})
+                    Assert-MockCalled -CommandName New-AzureADApplication -Times 1 -Exactly -Scope It
+                    Assert-MockCalled -CommandName Set-AzureADApplication -Times 0 -Exactly -Scope It
+                }
+                It 'should update an existing azure application' {
+                    # Arrange
+                    $requiredResourceAccess = New-Object -TypeName "Microsoft.Open.AzureAD.Model.RequiredResourceAccess"
+                    $requiredResourceAccess.ResourceAccess = $existingResourceAccess,$newResourceAccess
+                    $returnApp = @{
+                        ObjectId = 1234
+                        ReplyUrls = New-Object System.Collections.Generic.List[string]
+                        RequiredResourceAccess = $requiredResourceAccess
+                    }
+    
+                    Mock -CommandName Get-AzureADApplication { return $returnApp }
+                    Mock -CommandName Connect-AzureAD {}
+                    Mock -CommandName New-AzureADApplication {}
+                    Mock -CommandName Set-AzureADApplication {}
+                    Mock -CommandName Get-AzureADServicePrincipal {return $returnPrincipal}
+    
+                    # Act
+                    $app = New-FabricAzureADApplication -appName "app" -replyUrls @(@{name="url"}) -permissions $requiredResourceAccess
+    
+                    # Assert
+                    Assert-MockCalled -CommandName New-AzureADApplication -Times 0 -Exactly -Scope It
+                    Assert-MockCalled -CommandName Set-AzureADApplication -Times 1 -Exactly -Scope It
+                    $app.RequiredResourceAccess.ResourceAccess.Count | Should -eq 2
+                    $app.RequiredResourceAccess.ResourceAccess | Should -contain $existingResourceAccess
+                    $app.RequiredResourceAccess.ResourceAccess | Should -contain $newResourceAccess
+                }
+            }
+        }
+    }
+
+    Describe 'Remove-IdentityClaimsIssuerFromTenantsList' {
+        Context 'Happy path' {
+            BeforeEach {
+                $tenantsArray = @(
+                    @{
+                        name = "name1"
+                        alias = "alias1"
+                    },
+                    @{
+                        name = "name2"
+                        alias = "alias2"
+                    },
+                    @{
+                        name = "name3"
+                        alias = "alias3"
+                    }
+                )
+            }
+            It 'Should remove the claims issuer tenant provided' {
+                # Arrange
+                $claimsIssuerName = "name2"
+    
+                # Act
+                $resultTenants += Remove-IdentityClaimsIssuerFromTenantsList -tenants $tenantsArray -claimsIssuerName $claimsIssuerName
+                # Assert
+                $resultTenants.Count | Should -eq 2
+                $resultTenants[0].name | Should -eq "name1"
+                $resultTenants[1].name | Should -eq "name3"
+            }
+    
+            It 'Should remove the claims issuer tenant provided if only the claims issuer exists' {
+                $tenants = @(
+                    @{
+                        name = "name1"
+                        alias = "alias1"
+                    }
+                )
+                # Arrange
+                $claimsIssuerName = "name1"
+    
+                # Act
+                $resultTenants += Remove-IdentityClaimsIssuerFromTenantsList -tenants $tenants -claimsIssuerName $claimsIssuerName
+                # Assert
+                $resultTenants.Count | Should -eq 0
+            }
+    
+            It 'Should not remove any tenant if the claims issuer does not exist' {
+                # Arrange
+                $claimsIssuerName = "anyname"
+    
+                # Act
+                $resultTenants += Remove-IdentityClaimsIssuerFromTenantsList -tenants $tenantsArray -claimsIssuerName $claimsIssuerName
+                # Assert
+                $resultTenants.Count | Should -eq 3
+                $resultTenants[0].name | Should -eq "name1"
+                $resultTenants[1].name | Should -eq "name2"
+                $resultTenants[2].name | Should -eq "name3"
+            }
+        }
+    }
+    
 }
 
 Remove-Module Install-IdPSS-Utilities -Force
