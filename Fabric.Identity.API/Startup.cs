fcb188a89e84980fe12464c3c34b28beac7df4bf
Ben Skeen <benjamin.skeen@healthcatalyst.com>
2019-10-24 16:04:00 -0600

    I added a space to get the build to run again.

--- Fabric.Identity.API/Startup.cs
+++ Fabric.Identity.API/Startup.cs
@@ -69,7 +69,7 @@ namespace Fabric.Identity.API
             }
         }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the container. 
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
